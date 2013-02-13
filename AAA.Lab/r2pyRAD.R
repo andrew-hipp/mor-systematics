6c96730966b37bc8318b51e0c5b753ee54d77960
andrew.hipp@gmail.com <andrew.hipp@gmail.com@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2013-02-13 18:23:30 +0000

    another update to overlap.report

--- AAA.Lab/r2pyRAD.R
+++ AAA.Lab/r2pyRAD.R
@@ -176,7 +176,7 @@ overlap.report <- function(dat, repPattern = "_re", origPattern = "_h") {
   if(class(dat) == 'summary.pyRAD.loci') dat <- dat$inds.mat
   reps <- grep(repPattern, row.names(dat), fixed = TRUE, value = TRUE)
   orig <- gsub(repPattern, origPattern, reps, fixed = TRUE)
-  outCols <- c("Original", "Replicate", "Intersection", "Union", "Not intersection", "Overlap proportion", "Original loci replicated")
+  outCols <- c("Original", "Replicate", "Intersection", "Union", "Not intersection", "Overlap proportion", "Original loci replicated", "Increase, original to replicate")
   out <- matrix(NA, nrow = length(orig), ncol = length(outCols), dimnames = list(orig, outCols))
   for(i in 1:length(orig)) {
     message(paste("Doing names", orig[i]))
@@ -188,6 +188,7 @@ overlap.report <- function(dat, repPattern = "_re", origPattern = "_h") {
   out[, "Not intersection"] <- out[, "Union"] - out[, "Intersection"]
   out[, "Overlap proportion"] <- round(out[, "Intersection"] / out[, "Union"], 3)
   out[, "Original loci replicated"] <- round(out[, "Intersection"] / out[, "Original"], 3)
+  out[, "Increase, original to replicate"] <- round(out[, "Replicate"]/ round[out, "Original"], 3)
   return(out)
   }
 
