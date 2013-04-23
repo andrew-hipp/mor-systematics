7ad8b454e0579caa944b139e8379d5dfd0da2831
andrew.hipp <andrew.hipp@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2013-04-23 04:23:12 +0000

    adding code to check blast2GO work

---
+++ Hipp/2013.IOS/MPE.2013.analyses.2.blast2go.R
@@ -0,0 +1,31 @@
+## Checking Blast2GO results
+## A Hipp, 22 April 2013
+
+## read in the datasets that Elisabeth sent
+oak.blasts = lapply(dir(patt = 'txt'), read.delim)
+names(oak.blasts) = c('quercus.all', 'est.others', 'ref.seq.RNA')
+for(i in names(oak.blasts)) oak.blasts[[i]]$data.source <- i
+concat.fields <- c("query", "subject", "subject.accessions", "evalue", "query.start", 
+                  "query.end", "subject.start", "subject.end", "bit.score", "Seq..Description", "Seq..Length", 
+                  "X.Hits", "min..eValue", "mean.Similarity", "X.GOs", "GOs", "Enzyme.Codes", 'data.source')
+oak.blasts.new <- rbind(oak.blasts[[1]][concat.fields], oak.blasts[[2]][concat.fields])
+oak.blasts.new <- rbind(oak.blasts.new, oak.blasts[[3]][concat.fields])
+oak.blasts <- oak.blasts.new
+rm(oak.blasts.new)
+
+unique.loci <- sort(unique(oak.blasts$query))
+stats.of.interest <- c('number of hits', 'number of unique descriptions', 'descriptions concatenated', 'QUERCUS', 'EST.OTHERS', 'REF.SEQ.RNA')
+locus.summary.mat <- matrix(NA, length(unique.loci), length(stats.of.interest), dimnames = list(unique.loci, stats.of.interest))
+for(i in unique.loci) {
+  # browser()
+  temp <- oak.blasts[oak.blasts$query == i, ]
+  locus.summary.mat[i, 'QUERCUS'] <- sum(temp$data.source == 'quercus.all') 
+  locus.summary.mat[i, 'EST.OTHERS'] <- sum(temp$data.source == 'est.others') 
+  locus.summary.mat[i, 'REF.SEQ.RNA'] <- sum(temp$data.source == 'ref.seq.RNA') 
+  locus.summary.mat[i, 1] = dim(temp)[1]
+  temp.desc <- unique(temp$Seq..Description)
+  missing <- unique(c(grep("NA", temp.desc, fixed = TRUE), grep("N/A", temp.desc, fixed = TRUE)))
+  if(length(missing) > 0) temp.desc <- temp.desc[-missing]
+  locus.summary.mat[i, 2] <- length(temp.desc)
+  locus.summary.mat[i, 3] <- paste(temp.desc, collapse = "||")
+  }
