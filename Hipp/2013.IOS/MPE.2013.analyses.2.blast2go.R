0697657da0a5463ccb15e12b57f4bbabe05beaca
andrew.hipp <andrew.hipp@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2013-04-25 15:44:59 +0000

    updating the Blast2GO parsing

--- Hipp/2013.IOS/MPE.2013.analyses.2.blast2go.R
+++ Hipp/2013.IOS/MPE.2013.analyses.2.blast2go.R
@@ -2,8 +2,8 @@
 ## A Hipp, 22 April 2013
 
 ## read in the datasets that Elisabeth sent
-oak.blasts = lapply(dir(patt = 'txt'), read.delim)
-names(oak.blasts) = c('quercus.all', 'est.others', 'ref.seq.RNA')
+oak.blasts = lapply(dir('./2013-04-09.data/', full = T), read.delim)
+names(oak.blasts) = c('est.others', 'quercus.all', 'ref.seq.RNA')
 for(i in names(oak.blasts)) oak.blasts[[i]]$data.source <- i
 concat.fields <- c("query", "subject", "subject.accessions", "evalue", "query.start", 
                   "query.end", "subject.start", "subject.end", "bit.score", "Seq..Description", "Seq..Length", 
@@ -14,7 +14,8 @@ oak.blasts <- oak.blasts.new
 rm(oak.blasts.new)
 
 unique.loci <- sort(unique(oak.blasts$query))
-stats.of.interest <- c('number of hits', 'number of unique descriptions', 'descriptions concatenated', 'QUERCUS', 'EST.OTHERS', 'REF.SEQ.RNA')
+stats.of.interest <- c('number of hits', 'number of unique descriptions', 'descriptions concatenated', 
+                       'number of unique GOs', 'GO terms concatenated', 'QUERCUS', 'EST.OTHERS', 'REF.SEQ.RNA')
 locus.summary.mat <- matrix(NA, length(unique.loci), length(stats.of.interest), dimnames = list(unique.loci, stats.of.interest))
 for(i in unique.loci) {
   # browser()
@@ -26,6 +27,11 @@ for(i in unique.loci) {
   temp.desc <- unique(temp$Seq..Description)
   missing <- unique(c(grep("NA", temp.desc, fixed = TRUE), grep("N/A", temp.desc, fixed = TRUE)))
   if(length(missing) > 0) temp.desc <- temp.desc[-missing]
+  temp.desc <- temp.desc[temp.desc != "0"]
   locus.summary.mat[i, 2] <- length(temp.desc)
   locus.summary.mat[i, 3] <- paste(temp.desc, collapse = "||")
+  GOs <- sort(unlist(strsplit(as.character(temp$GOs), "; ")))
+  GOs <- GOs[GOs != ""]
+  locus.summary.mat[i, 4] <- length(GOs)
+  locus.summary.mat[i, 5] <- paste(GOs, collapse = "||")
   }
