c12d1c7d95f60344ec3e1641fdad563593ac25fd
andrew.hipp <andrew.hipp@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2013-05-16 19:15:15 +0000

    making trial analyses is now a set of functions

--- AAA.Lab/r2pyRAD.R
+++ AAA.Lab/r2pyRAD.R
@@ -220,20 +220,22 @@ filter.by <- function(dat, taxa) {
   return(names(which(apply(dat.mat, 2, sum) == length(taxa))))
   }
 
-hyb.test.2830 <- hybrid.test(dat = test.18.v2.summary, parents = c('>2830D', '>2893G1'), 
+hyb.test.2830 <- function() {hybrid.test(dat = test.18.v2.summary, parents = c('>2830D', '>2893G1'), 
                                 f1s = c('>2830Dx2893G1A', '>2830Dx2893G1B','>2830Dx2893G1C','>2830Dx2893G1D', '>2830Dx2893G1E', '>2893Gx2830D1A', '>2893Gx2830D1C'))
-								 
-hyb.test.2816 <- hybrid.test(dat = test.18.v2.summary, parents = c('>2816', '>2893G1'),
+							}	
+							
+hyb.test.2816 <- function() {hybrid.test(dat = test.18.v2.summary, parents = c('>2816', '>2893G1'),
                                 f1s = c(">2816x2893G1E", ">2816", ">2816x2893G1B", ">2816x2893G1C", ">2816x2893G1D", ">2816x2893G1A"))
-
-hyb.trial.allOffspring.2816 <- hybrid.test(dat = test.18.v2.summary, parents = c('>2816', '>2893G1'),
+							}
+							
+hyb.trial.allOffspring.2816 <- function() {hybrid.test(dat = test.18.v2.summary, parents = c('>2816', '>2893G1'),
                                 f1s = c(">2816x2893G1E", ">2816x2893G1B", ">2816x2893G1C", ">2816x2893G1D", ">2816x2893G1A",
 								'>2830Dx2893G1A', '>2830Dx2893G1B','>2830Dx2893G1C','>2830Dx2893G1D', '>2830Dx2893G1E', '>2893Gx2830D1A', '>2893Gx2830D1C'))
-
-hyb.trial.allOffspring.2830 <- hybrid.test(dat = test.18.v2.summary, parents = c('>2830D', '>2893G1'),
+							}
+hyb.trial.allOffspring.2830 <- function() {hybrid.test(dat = test.18.v2.summary, parents = c('>2830D', '>2893G1'),
                                 f1s = c(">2816x2893G1E", ">2816x2893G1B", ">2816x2893G1C", ">2816x2893G1D", ">2816x2893G1A",
 								'>2830Dx2893G1A', '>2830Dx2893G1B','>2830Dx2893G1C','>2830Dx2893G1D', '>2830Dx2893G1E', '>2893Gx2830D1A', '>2893Gx2830D1C'))
-
+							}
 								
 hybrid.test <- function(dat, parents, f1s)								 
 ### TO DO (3/14/2013, AH and AMELirio):
