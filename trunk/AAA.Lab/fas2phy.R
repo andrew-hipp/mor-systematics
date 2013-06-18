35b5fd74d1acea1912c489ecf8f60b6b3bc5c432
andrew.hipp@gmail.com <andrew.hipp@gmail.com@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2013-06-18 22:21:36 +0000

    updating fas2phy.R

--- trunk/AAA.Lab/fas2phy.R
+++ trunk/AAA.Lab/fas2phy.R
@@ -1,3 +1,20 @@
 ## batch process a bunch of fasta files into phylip, make analysis batch file
 ## A Hipp, 2013-06-18
 
+fas2phy <- function(working.dir = choose.dir(), pattern = '.fas', ignore = 'none', suffix = '.phy', analysis.subdir = paste('raxml.', format(Sys.time(), "%Y-%m-%d"), sep = ''), rax.batch.filename = paste('raxml.', format(Sys.time(), "%Y-%m-%d.bat"), sep = '')) {
+  require(ape)
+  original.dir <- getwd()
+  setwd(working.dir)
+  files <- dir(patt = pattern)
+  if(ignore != 'none') files <- files[!files %in% ignore]
+  if(!analysis.subdir %in% dir()) dir.create(analysis.subdir)
+  lapply(files, function(x) write.dna(read.dna(x, format = 'fasta'), paste(analysis.subdir, '/', x, suffix, sep = ''), 
+                                      format = 'sequential', nbcol = -1, colsep = ''))
+  setwd(analysis.subdir)
+  subsubdirs <- sapply(files, function(x) strsplit(x, pattern, fixed = T)[[1]][1])
+  lapply(subsubdirs, dir.create)
+  rax.out <- paste('raxmlHPC-PTHREADS -f a -x 12345 -m GTRCAT -# 200 -T 2 -s', paste(files, suffix, sep = ''), '-w', subsubdirs, '-n', paste(files, '.raxd.tre', sep = ''))
+  writeLines(rax.out, rax.batch.filename)
+  setwd(original.dir)
+  return('done!')
+  }
