96bd86208424272f34fcbb2fd6ce049e7b3928fd
andrew.hipp <andrew.hipp@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2013-06-30 15:47:54 +0000

    resolved conflicts; edited fas2phy

--- AAA.Lab/fas2phy.R
+++ AAA.Lab/fas2phy.R
@@ -1,12 +1,17 @@
 ## batch process a bunch of fasta files into phylip, make analysis batch file
 ## A Hipp, 2013-06-18
 
-fas2phy <- function(working.dir = choose.dir(), pattern = '.fas', ignore = 'none', suffix = '.phy', analysis.subdir = paste('raxml.', format(Sys.time(), "%Y-%m-%d"), sep = ''), rax.batch.filename = paste('raxml.', format(Sys.time(), "%Y-%m-%d.bat"), sep = '')) {
+get.files <- function(working.dir = choose.dir(), get.patt = '.fas', ignore.patt = NA, ...) {
+  out <- list(working.dir = working.dir, files = dir(working.dir,patt = get.patt, full = TRUE))
+  if(!is.na(ignore.patt[1])) out$files <- out$files[-grep(paste(ignore.patt, collapse = '|'), out$files, ...)]
+  return(out)
+  }
+
+fas2phy <- function(fileList = get.files(), suffix = '.phy', analysis.subdir = paste('raxml.', format(Sys.time(), "%Y-%m-%d"), sep = ''), rax.batch.filename = paste('raxml.', format(Sys.time(), "%Y-%m-%d.bat"), sep = '')) {
   require(ape)
   original.dir <- getwd()
-  setwd(working.dir)
-  files <- dir(patt = pattern)
-  if(ignore != 'none') files <- files[!files %in% ignore]
+  files <- fileList$files
+  setwd(fileList$working.dir)
   if(!analysis.subdir %in% dir()) dir.create(analysis.subdir)
   lapply(files, function(x) write.dna(read.dna(x, format = 'fasta'), paste(analysis.subdir, '/', x, suffix, sep = ''), 
                                       format = 'sequential', nbcol = -1, colsep = ''))
@@ -19,6 +24,23 @@ fas2phy <- function(working.dir = choose.dir(), pattern = '.fas', ignore = 'none
   return('done!')
   }
 
-concat.DNA <- function(dat) {
+concat.dna <- function(file.list, report = TRUE, delim = '.', ...) {
 ## dat is a list of DNA objects
-}
+  require(ape)
+  dat <- lapply(file.list, read.dna, format = 'fasta')
+  names(dat) = sapply(file.list, function(x) strsplit(x, delim, fixed = TRUE)[[1]][1])
+  if(report) dna.summary <- dna.summary.report(structure(lapply(dat, row.names), names = names(dat)))
+  out <- do.call('cbind.DNAbin', c(dat, fill.with.gaps = TRUE))
+  if(report) out <- list(dna = out, dna.summary = dna.summary)
+  return(out)
+  }
+
+dna.summary.report <- function(names.list) {
+## takes a list of names and makes a matrix for who has what; sums by columns and rows
+  all.names <- sort(unique(unlist(names.list)))
+  out <- do.call('cbind', lapply(names.list, function(x) all.names %in% x))
+  out <- cbind(out, totals = apply(out, 1, sum))
+  out <- rbind(out, totals = apply(out, 2, sum))
+  dimnames(out) <- list(c(all.names, 'total'), c(names(names.list), 'total'))
+  return(out)
+  }
