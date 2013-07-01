bc3700a4546babfd3da32633779ec978e128e834
andrew.hipp <andrew.hipp@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2013-07-01 15:06:48 +0000

    updating cariceae code

--- cariceae/R/cariceae.trees.R
+++ cariceae/R/cariceae.trees.R
@@ -6,6 +6,17 @@ top12 <- c('ITS', 'ETS','trnLF','rbcL','matK','18S','trnK','rpoC1','rps16','psbA
 top5 <- c('ITS', 'ETS','trnLF','rbcL','matK')
 dregs <- c('trnK','rpoC1','rps16','psbA','atpF','rpoB')
 
+summarize.gene.coverage <- function(datMat = cariceae.concat.4genes.summary, extra.left = 2, ...) {
+  genes.no = dim(datMat)[2]
+  inds.no = dim(datMat)[1]
+  par(mar = c(5, 4 + extra.left, 4, 2) + 0.1)
+  plot(0, type = "n", xlim = c(1,inds.no), ylim = c(0, genes.no+1), xlab = 'Individuals sequenced', ylab = '', axes = FALSE)
+  axis(2, at = seq(1:genes.no), labels = names(datMat), tick = FALSE, las = 1)
+  axis(1)
+  for(i in 1:genes.no) points(which(datMat[1:inds.no, i] == 1), rep(i, sum(datMat[1:inds.no, i])), col = i, ...)
+  return('done')
+  }
+  
 ## tree traversal and counting
 tip.spp <- function(tree, delim = '_') {
 ## finds spp at tips, assuming first two elements are the genus and sp epithet
