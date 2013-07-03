8ea6ac3ca03dc716d53a4585b9ab24f48403cc9e
andrew.hipp <andrew.hipp@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2013-07-03 02:11:48 +0000

    adding XLS files editing WCM.R and cariceae.trees.R

--- trunk/cariceae/R/cariceae.trees.R
+++ trunk/cariceae/R/cariceae.trees.R
@@ -10,7 +10,7 @@ summarize.gene.coverage <- function(datMat = cariceae.concat.4genes.summary, ext
   genes.no = dim(datMat)[2]
   inds.no = dim(datMat)[1]
   par(mar = c(5, 4 + extra.left, 4, 2) + 0.1)
-  plot(0, type = "n", xlim = c(1,inds.no), ylim = c(0, genes.no+1), xlab = 'Individuals sequenced', ylab = '', axes = FALSE)
+  plot(0, type = "n", xlim = c(1,inds.no), ylim = c(0, genes.no+1), xlab = 'Individual sequenced [number]', ylab = '', axes = FALSE)
   axis(2, at = seq(1:genes.no), labels = names(datMat), tick = FALSE, las = 1)
   axis(1)
   for(i in 1:genes.no) points(which(datMat[1:inds.no, i] == 1), rep(i, sum(datMat[1:inds.no, i])), col = i, ...)
