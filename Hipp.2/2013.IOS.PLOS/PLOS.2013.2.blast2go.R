96fea6642474acd4b501af37f5fc6d6cb85b379c
andrew.hipp <andrew.hipp@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2013-10-01 10:32:01 +0000

    renaming

---
+++ Hipp.2/2013.IOS.PLOS/PLOS.2013.2.blast2go.R
@@ -0,0 +1,38 @@
+## Checking Blast2GO results
+## A Hipp, 22 April 2013
+
+## read in the datasets that Elisabeth sent
+oak.blasts = lapply(dir('./2013-04-09.data/', full = T), read.delim, as.is = T)
+names(oak.blasts) = c('est.others', 'quercus.all', 'ref.seq.RNA')
+for(i in names(oak.blasts)) oak.blasts[[i]]$data.source <- i
+concat.fields <- c("query", "subject", "subject.accessions", "evalue", "query.start", 
+                  "query.end", "subject.start", "subject.end", "bit.score", "Seq..Description", "Seq..Length", 
+                  "X.Hits", "min..eValue", "mean.Similarity", "X.GOs", "GOs", "Enzyme.Codes", 'data.source')
+oak.blasts.new <- rbind(oak.blasts[[1]][concat.fields], oak.blasts[[2]][concat.fields])
+oak.blasts.new <- rbind(oak.blasts.new, oak.blasts[[3]][concat.fields])
+oak.blasts <- oak.blasts.new
+rm(oak.blasts.new)
+
+unique.loci <- sort(unique(oak.blasts$query))
+stats.of.interest <- c('number of hits', 'number of unique descriptions', 'descriptions concatenated', 
+                       'number of unique GOs', 'GO terms concatenated', 'QUERCUS', 'EST.OTHERS', 'REF.SEQ.RNA')
+locus.summary.mat <- matrix(NA, length(unique.loci), length(stats.of.interest), dimnames = list(unique.loci, stats.of.interest))
+
+for(i in unique.loci) {
+  # browser()
+  temp <- oak.blasts[oak.blasts$query == i, ]
+  locus.summary.mat[i, 'QUERCUS'] <- sum(temp$data.source == 'quercus.all') 
+  locus.summary.mat[i, 'EST.OTHERS'] <- sum(temp$data.source == 'est.others') 
+  locus.summary.mat[i, 'REF.SEQ.RNA'] <- sum(temp$data.source == 'ref.seq.RNA') 
+  locus.summary.mat[i, 1] = dim(temp)[1]
+  temp.desc <- unique(temp$Seq..Description)
+  missing <- unique(c(grep("NA", temp.desc, fixed = TRUE), grep("N/A", temp.desc, fixed = TRUE)))
+  if(length(missing) > 0) temp.desc <- temp.desc[-missing]
+  temp.desc <- temp.desc[temp.desc != "0"]
+  locus.summary.mat[i, 2] <- length(temp.desc)
+  locus.summary.mat[i, 3] <- paste(temp.desc, collapse = "||")
+  GOs <- sort(unique(unlist(strsplit(as.character(temp$GOs), "; "))))
+  GOs <- GOs[!GOs %in% c("", "-")]
+  locus.summary.mat[i, 4] <- length(GOs)
+  locus.summary.mat[i, 5] <- paste(GOs, collapse = "||")
+  }
---
+++ Hipp.2/durand.r
@@ -0,0 +1,48 @@
+﻿## for linking RADs to Durand linkage map
+
+loadDat <- FALSE
+
+if(loadDat) durand <- read.delim('durand-2010')
+
+seqsFromDurand <- function(dat = durand[durand$linkage.group.assignement. != "NO", ]) {
+  # 1. make a table with clone as the row.name, then SSR.ID as the field
+  # 2. search all the clones and export those as a fasta file
+  # 3. blast against the clones fasta file, export b6 file
+  # 4. read b6 file back in, get the linkage group assignment for each marker
+  # 5. hist of linkage groups by individual
+  # 6. filter phylogeny matrix by these loci, si possible.
+  
+  # 1
+  cloneMat = matrix(NA,0,1,dimnames = list(NULL, "SSR.ID"))
+  for(i in 1:dim(dat)[1]) {
+    clones = sapply(strsplit(unlist(strsplit(as.character(dat$EST.ID[i]), ",")), ".", T), function(x) gsub(" ", "", x[1]))
+    cloneMat = rbind(cloneMat, matrix(dat$SSR.ID[i], length(clones), 1, dimnames = list(clones, "SSR.ID")))
+	} # not robust... will fail if any clone names are non-unique
+	
+  # 2
+  fastaFile <- entrez(row.names(cloneMat))
+  write.dna(fastaFile, format = "fasta", file = "trial.fas")
+  }
+
+linkBack <- function(b6.files = durand.b6.files, fileNames = durand.b6.file.names, refDat = durand[durand$linkage.group.assignement. != "NO", ], threshold = -14) {
+  inDat <- lapply(b6.files, read.delim, header = F, colClasses = "character")
+  names(inDat) <- fileNames
+  outDat <- sapply(inDat, function(x) apply(x, 1, function(y) refDat[grep(y[[2]], refDat[, 'EST.ID']), 'linkage.group.assignement.'][1]))
+  for(i in names(inDat)) outDat[[i]] <- cbind(inDat[[i]], outDat[[i]])
+  if(!is.na(threshold)) outDat <- list(durand.mats = outDat, linkage.mats = linkMats(outDat, threshold))
+  return(outDat)
+  }
+
+linkMats <- function(durand.dat, threshold, lgNo = 12) {
+  #takes linkBack$durand.mats and make matrix of linkage groups for each one
+  if(class(durand.dat) != "list") durand.dat <- list(durand.dat)
+  outMat <- matrix(NA, nrow = length(durand.dat), ncol = lgNo, dimnames = list(names(durand.dat), paste("LG", seq(lgNo), sep = "")))
+  for(i in 1:length(durand.dat)) {
+    dat <- split(durand.dat[[i]], durand.dat[[i]][[1]]) # splits into matrices with all rows of a given name
+	dat <- dat[log10(sapply(dat, function(x) min(as.numeric(x[, 11])))) <= threshold] # filters out only those matrices with a fit better than or equal to threshold
+	lg.vector <- sapply(dat, function(x) as.integer(as.character(x[x[, 12] == min(x[, 12]), 13][1])))
+	outMat[i, ] <- sapply(1:12, function(x) sum(lg.vector == x))
+	}
+  return(outMat)
+  }
+	
