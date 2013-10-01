9867534dc7a7e1a28c13dec8b6a7cb9aa79c1015
andrew.hipp <andrew.hipp@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2013-10-01 10:33:15 +0000

    deleting unwatched 'hipp'

--- trunk/Hipp/srHelper.R
+++
@@ -1,5 +0,0 @@
-﻿# helper functions for dealing with short-read data
-# Andrew Hipp, December 2012
-
-str2mat <- function(seqs) t(sapply(seqs, function(x) strsplit(x, "")[[1]])) # turns a vector of sequences into a matrix, one nucleotide per cell
-
