0cde1398159818a6df1b92928cd262c0cbcbdc5e
andrew.hipp@gmail.com <andrew.hipp@gmail.com@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2013-11-13 16:43:46 +0000

    updating RADS files and file structure

---
+++ rads/unused/genTrees.paupBlock.R
@@ -0,0 +1,23 @@
+﻿  if(software[1] == 'paup') {
+    write.nexus(treeset, file = paste(filebase, '.rtrees.tre', sep = ''))
+    write.nexus(x, file = paste(filebase, '.optimal.tre', sep = ''))
+	}
+
+
+  ## write paup block
+  if(software[1] == 'paup') {
+    paup.out <- file(description = paste(filebase, '.paupBlock.nex', sep = ''), open = "w")
+    writeLines("#NEXUS", paup.out)
+    writeLines(paste("[PAUP block written using genTrees function in R,", date(), "]"), paup.out)
+    writeLines("\nBEGIN PAUP;\n", paup.out)
+    writeLines("  exclude constant /only;", paup.out)
+    writeLines("  increase = auto autoclose = yes;", paup.out)
+    writeLines("  cleartrees;", paup.out)
+    writeLines(paste("  gettrees file =", paste(filebase, '.optimal.tre', sep = ''), ";"), paup.out)
+    writeLines(paste("  lscores all / sitelikes = yes scorefile =", paste(filebase, '.optimal.scores', sep = ''), ";"), paup.out)
+    writeLines("\n  cleartrees;", paup.out)
+    writeLines(paste("  gettrees file =", paste(filebase, '.rtrees.tre', sep = ''), ";"), paup.out)
+    writeLines(paste("  lscores all / sitelikes = yes scorefile =", paste(filebase, '.rtrees.scores', sep = ''), ";"), paup.out)
+    writeLines("\nend;", paup.out)
+    close(paup.out)
+	}
