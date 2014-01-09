8e26ff4babf8b18febd507a36a504e3ba4452523
ahipp@mortonarb.org <ahipp@mortonarb.org@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2014-01-09 14:34:15 +0000

    making plot.swul default to doing the difference between best loci and worst loci as the third column of panels

--- rads/swul.R
+++ rads/swul.R
@@ -54,7 +54,7 @@ get.raxml.siteLikelihoods <- function(x)  {
     return(lnL)
 	}
 
-plot.swulLikelihoods <- function(x, scalar = 1, percentile = c(0.025, 0.975), output = c('jpg'), bw.scalar = NA, scale.by = c('numTaxa','sd', 1), opt = c('diff', 'both'), ...) {
+plot.swulLikelihoods <- function(x, scalar = 1, percentile = c(0.025, 0.975), output = c('jpg'), bw.scalar = NA, scale.by = c('numTaxa','sd', 1), opt = c('diff', 'both'), add.opt = T, ...) {
 ## each tree is a data point
 ## X: tree likelihood
 ## Y: 
@@ -84,15 +84,18 @@ plot.swulLikelihoods <- function(x, scalar = 1, percentile = c(0.025, 0.975), ou
   if(class(scale.by) == 'numeric') dotSizes.best <- dotSizes.worst <- scale.by
   layout(matrix(1:3, 1, 3))
   plot(X, Y.best, cex = dotSizes.best, xlab = 'Tree log-likelihood', ylab = paste('Number of loci for which tree is above the',percentile[2],'quantile'), ylim = range(c(Y.best, Y.worst)), pch = 21, bg = 'black', col = 'gray')
+  if(add.opt) points(X[1], Y.best[1], cex = dotSizes.best, pch = 21, col = 'gray', bg = 'yellow')
   plot(X, Y.worst, cex = dotSizes.worst, xlab = 'Tree log-likelihood', ylab = paste('Number of loci for which tree is below the',percentile[1], 'quantile'), ylim = range(c(Y.best, Y.worst)), pch = 21, bg = 'red', col = 'black') 
+  if(add.opt) points(X[1], Y.worst[1], cex = dotSizes.worst, pch = 21, col = 'black', bg = 'yellow')
   if(opt[1] == 'both') {
     plot(X, Y.best, cex = dotSizes.best * (bw.scalar / scalar), xlab = 'Tree log-likelihood', ylab = 'Number of loci for which tree is above quantile (B) or below quantile (W)', ylim = range(c(Y.best, Y.worst)), pch = 21, bg = 'black', col = 'gray')
     points(X, Y.worst, cex = dotSizes.worst * (bw.scalar / scalar), bg = 'red', pch = 21, col = 'black')
     segments(X, Y.best, X, Y.worst, lty = 'dashed')
 	}
   else {
-    plot(out[, c('lnL', 'difference')], pch = 21, col = 'grey', bg = 'black', cex = 2)
-    abline(h = quantile(out[, c('difference')], c(0.025, 0.975)), lty = 'dashed')
+    plot(out[, c('lnL', 'difference')], pch = 21, col = 'grey', bg = 'black', cex = 2, xlab='Tree log-likelihood', ylab = 'Number of loci favoring minus number disfavoring tree')
+    points(out[1, 'lnL'], out[1, 'difference'], pch = 21, col = 'grey', bg = 'yellow', cex = 2)
+	abline(h = quantile(out[, c('difference')], c(0.025, 0.975)), lty = 'dashed')
     v.bad <- which(out[, c('difference')] < quantile(out[, c('difference')], c(0.025, 0.975))[1])
     v.good <- which(out[, c('difference')] > quantile(out[, c('difference')], c(0.025, 0.975))[2])
     text(out[v.good, c('lnL', 'difference')], labels = names(v.good), pos = 2)
