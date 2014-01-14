4e11f8e9e76ac8c4985ced47a0bc81da7ed4a2df
ahipp@mortonarb.org <ahipp@mortonarb.org@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2014-01-14 15:41:27 +0000

    getting the exports to work for SWUL

--- trunk/rads/unique.multiPhylo.R
+++ trunk/rads/unique.multiPhylo.R
@@ -1,18 +1,18 @@
-unique.multiPhylo <- function (x, incomparables = FALSE, use.edge.length = FALSE, use.tip.label = TRUE, ...) {
+unique.multiPhylo <- function (x, incomparables = FALSE, use.edge.length = FALSE, use.tip.label = TRUE, verbose = FALSE, ...) {
     ## edited 2014-01-13, AH
     n <- length(x)
     keep <- 1L
 	tree.vector <- integer(n) # this is a vector telling which new tree each old tree belongs to
     tree.vector[1] <- 1
 	for (i in 2:n) {
-        message(paste('--- Doing tree', i))
+        if(verbose) message(paste('--- Doing tree', i))
         already.seen <- FALSE
         tree.vector[i] <- length(keep) + 1 # defaults to its own tree unless this is changed
 		for (s in 1:length(keep)) { # this line changed...
 			if (all.equal(x[[keep[s]]], x[[i]], use.edge.length = use.edge.length, use.tip.label = use.tip.label, ...)) ### and this line
                 {
                 already.seen <- TRUE
-                message(paste('breaking out of tree', i, 'which is not unique'))
+                if(verbose) message(paste('breaking out of tree', i, 'which is not unique'))
                 tree.vector[i] <- s
 				break
             }
--- trunk/rads/workflows.pyRAD.R
+++ trunk/rads/workflows.pyRAD.R
@@ -2,24 +2,32 @@
 ## from the r2pyRAD library
 ## Andrew Hipp, The Morton Arboretum, 2014
 
-gen.RAD.loci.datasets <- function(rads, trees = 'none', loci = 'all', taxa = 'all', minTaxa = 3, onlyVariable = TRUE, fileBase = format(Sys.time(), "rads.%Y-%m-%d")) {
-  if(!fileBase %in% dir()) dir.create(fileBase) # defaults to making a directory to hold all the files
+gen.RAD.loci.datasets <- function(rads, trees = 'none', loci = 'all', taxa = 'all', minTaxa = 3, onlyVariable = TRUE, fileBase = format(Sys.time(), "rads.%Y-%m-%d"), splitInto = 8) {
+  if(!fileBase %in% dir()) lapply(paste(fileBase, ".", (0:splitInto), sep = ''), dir.create) # defaults to making a directory to hold all the files
   if(loci[1] == "all") loci <- unique(rads$locus.index)[unique(rads$locus.index) != ""]
   if(taxa[1] == "all") taxa <- unique(rads$tips)[gsub('/', '', unique(rads$tips), fixed = TRUE) != ''] # gets rid of the mock tip that is left by the current version of read.pyRAD
   if(trees[1] != 'none') taxa <- intersect(taxa, trees[[1]]$tip.label)
   locus.set <- subset.pyRAD.loci(rads, loci, taxa)
   locus.list <- locus.set$DNA[names(which(locus.set$ntaxa >= minTaxa))]
   if(onlyVariable) locus.list <- locus.list[names(which(locus.set$variable))]
+  if(trees[1] != 'none') tree.vector.matrix <- matrix(NA, nrow = length(locus.list), ncol = length(trees), dimnames = list(names(locus.list), names(trees)))
+  batch = 0
   for(i in names(locus.list)) {
-    message(paste('Writing', i))
+    if(batch == splitInto) batch <- 1
+	  else batch <- batch + 1
+	message(paste('Writing', i))
 	locus.taxa <- names(locus.list[[i]])[names(locus.list[[i]]) %in% taxa]
-	write.DNAStringSet(locus.list[[i]][locus.taxa], filename = paste(fileBase, '/', i, '.phy', sep = ''))
+	write.DNAStringSet(locus.list[[i]][locus.taxa], filename = paste(fileBase, '.', batch, '/', i, '.phy', sep = ''))
 	if(trees[1] != 'none') {
 	  trees.out <- lapply(trees, drop.tip, tip = trees[[1]]$tip.label[!trees[[1]]$tip.label %in% locus.taxa])
 	  class(trees.out) <- 'multiPhylo'
-	  write.tree(trees.out, file = paste(fileBase, '/', i, '.tre', sep = ''))
+	  trees.out <- unique(trees.out)
+	  message(paste('... kept', length(trees.out), 'trees'))
+	  write.tree(trees.out, file = paste(fileBase, '.', batch, '/', i, '.tre', sep = ''))
+	  tree.vector.matrix[i, ] <- attr(trees.out, "old2new.trees")
 	  }
   }
+  write.csv(tree.vector.matrix, paste(fileBase, '.0/', 'tree.vector.matrix.csv', sep = ''))
   ## AND CREATE BATCH FILE...
   ## AND EXPORT!
   ## There will also need to be a read function
