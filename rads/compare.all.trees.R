dd1c1d9035c66f3e5d571f6b15c9242a50fbe9ae
ahipp@mortonarb.org <ahipp@mortonarb.org@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2014-01-20 13:19:18 +0000

    partitioning out new files, and writing the likelihood import function; cleaning up genTrees.R so that it gives uniques -- but the unrooted trees are still misbehaving

---
+++ rads/compare.all.trees.R
@@ -0,0 +1,10 @@
+compare.all.trees <- function(treeset, ...) {
+  ntrees = length(treeset)
+  outmat = matrix(NA, ntrees, ntrees)
+  for(i in 1:ntrees) {
+   for(j in 1:i) {
+    outmat[i, j] <- all.equal(treeset[[i]], treeset[[j]], ...)
+	} # close j
+  } # close i
+  outmat
+  }
