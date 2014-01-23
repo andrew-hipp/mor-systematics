7d1f13e653c4d0fcee1546e8ee1c8c84fa837707
ahipp@mortonarb.org <ahipp@mortonarb.org@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2014-01-23 20:43:33 +0000

    working on logfile and getting names applied correctly

--- rads/get.raxml.likelihoods.R
+++ rads/get.raxml.likelihoods.R
@@ -1,6 +1,5 @@
 match.lnL.to.trees <- function(directory = 'getwd()', lnLprefix = 'RAxML_info.', lnLsuffix = '.lnL', treeIndexFile = 'tree.index.lines.txt', locus.names = NULL, ...) {
 ## updated 2014-01-23 to prune out files where no trees written -- TO TEST 
-  logfile = file(format(Sys.time(), "match.lnL.%Y-%m-%d.log.txt"), open = "a")
   treeIndex <- read.delim(paste(directory, '/', treeIndexFile, sep = ''), as.is = T, header = F, row.names = 1)
   if(is.null(locus.names)) locus.names <- row.names(treeIndex)
   lnL.list <- lapply(paste(directory, '/', lnLprefix, locus.names, lnLsuffix, sep = ''), get.raxml.treeLikelihoods)
@@ -32,13 +31,14 @@ get.raxml.treeLikelihoods <- function(x) {
 ## gets likelihoods from the RAxML_info file
 ## updated 2014-01-23 to deliver a fail if no trees written
     logfile = file(format(Sys.time(), "match.lnL.%Y-%m-%d.log.txt"), open = "a")
-	cat(paste('working on file', x, '\n'))
+	# cat(paste('working on file', x, '\n'), file = logfile)
 	fileIn <- readLines(x)
 	if(length(grep("Tree ", fileIn)) == 0) {
 	  cat('... file', x, 'had no trees in it.', '\n')
 	  return('FAIL')
 	  }
 	out <- as.double(sapply(grep("Tree ", fileIn, value=T), function(x) strsplit(x, ": ")[[1]][2]))
+	dev.off()
 	names(out) <- as.character(1:length(out))
 	out
 	}
