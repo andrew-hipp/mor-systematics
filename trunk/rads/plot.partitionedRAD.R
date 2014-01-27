ff1a1adf52c8ef5b7fcdc27abf8e366b70289c55
ahipp@mortonarb.org <ahipp@mortonarb.org@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2014-01-27 09:22:28 +0000

    ylabel fixed for overlap panel

--- trunk/rads/plot.partitionedRAD.R
+++ trunk/rads/plot.partitionedRAD.R
@@ -13,7 +13,7 @@ plot.rankedPartitionedRAD <- function(x,
     temp.ylab = switch(i, 
 	                   bestMat = 'Number of loci supporting tree',
 					   worstMat = 'Number of loci disfavoring tree',
-					   doubleCountMat = 'Number of loci excluded, overlap between support and disfavor'
+					   doubleCountMat = 'Loci overlapping, excluded'
 					   )
 	plot(x$trees.lnL, colSums(x[[i]]), xlab = 'Tree log-likelihood, summed over loci', ylab = temp.ylab, ...)
     points(x$trees.lnL[1], colSums(x[[i]])[1], pch = primeTreeCharacter, col = primeTreeColor)
