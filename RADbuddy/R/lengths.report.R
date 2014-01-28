c208d0f8ce0856fd7707b756e271ba4d930e4618
ahipp@mortonarb.org <ahipp@mortonarb.org@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2014-01-28 13:41:58 +0000

    continuing with documentation, fixing issues along the way; renaming to RADami

--- RADbuddy/R/lengths.report.R
+++ RADbuddy/R/lengths.report.R
@@ -5,17 +5,18 @@ function(dat, numtodo = 10, reportInterval = 2000, high.mem = TRUE) {
   last.lines <- dat$cons - 1
   num.loci <- length(last.lines)
   datSeqs <- as.character(dat$seqs)
-  if(high.mem) block.lengths <- sapply(datSeqs[last.lines][1:ifelse(numtodo<1,num.loci,numtodo)], function(x) nchar(as.character(x)))
+  last.locus <- ifelse(numtodo < 1, num.loci, numtodo)
+  if(high.mem) block.lengths <- sapply(datSeqs[last.lines][1:last.locus], function(x) nchar(as.character(x)))
   else {
     block.lengths = integer(0)
-	for(i in 1:num.loci) block.lengths = c(block.lengths, nchar(as.character(datSeqs[last.lines[i]])))
+	for(i in 1:last.locus) block.lengths = c(block.lengths, nchar(as.character(datSeqs[last.lines[i]])))
 	if(i / reportInterval - i %/% reportInterval == 0) {
-  	   message(paste('...', i, 'of', num.loci, 
- 	   '-- Estimated time remaining =', ((Sys.time() - start.time) / i) * (num.loci - i), attr(Sys.time() - start.time, 'units')
+  	   message(paste('...', i, 'of', last.locus, 
+ 	   '-- Estimated time remaining =', ((Sys.time() - start.time) / i) * (last.locus - i), attr(Sys.time() - start.time, 'units')
   	   ))
 	   }
 
 	}
-  names(block.lengths) <- dat$locus.index[last.lines]
+  names(block.lengths) <- dat$locus.index[last.lines][1:last.locus]
   return(block.lengths)
   }
