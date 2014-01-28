5a2f21b79a0036bdc8656a6d5f9b77233c3b49fe
ahipp@mortonarb.org <ahipp@mortonarb.org@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2014-01-28 14:06:42 +0000

    fixing overlap.report.R so that pairs of individuals are rows, not just the original individuals

--- trunk/RADbuddy/R/overlap.report.R
+++ trunk/RADbuddy/R/overlap.report.R
@@ -2,16 +2,18 @@ overlap.report <-
 function(dat, repPattern = "_re", origPattern = "_h") {
 ## reports on how replicate individuals fall out in the loci
   if(class(dat) == 'summary.pyRAD.loci') dat <- dat$inds.mat
+  if(class(dat) == 'pyRAD.loci') dat <- dat$radSummary$inds.mat
   reps <- grep(repPattern, row.names(dat), fixed = TRUE, value = TRUE)
   orig <- gsub(repPattern, origPattern, reps, fixed = TRUE)
+  pairNames <- paste(orig, reps, sep = ' and ')
   outCols <- c("Original", "Replicate", "Intersection", "Union", "Not intersection", "Overlap proportion", "Original loci replicated", "Increase, original to replicate")
-  out <- matrix(NA, nrow = length(orig), ncol = length(outCols), dimnames = list(orig, outCols))
+  out <- matrix(NA, nrow = length(orig), ncol = length(outCols), dimnames = list(pairNames, outCols))
   for(i in 1:length(orig)) {
-    message(paste("Doing names", orig[i]))
-	out[orig[i], "Original"] <- sum(dat[orig[i],])
-	out[orig[i], "Replicate"] <- sum(dat[reps[i],])
-	out[orig[i], "Intersection"] <- sum(colSums(dat[c(orig[i], reps[i]), ]) == 2, na.rm = T)
-	out[orig[i], "Union"] <- sum(colSums(dat[c(orig[i], reps[i]), ]) %in% 1:2, na.rm = T)
+    message(paste("Doing names", pairNames[i]))
+	out[pairNames[i], "Original"] <- sum(dat[orig[i],])
+	out[pairNames[i], "Replicate"] <- sum(dat[reps[i],])
+	out[pairNames[i], "Intersection"] <- sum(colSums(dat[c(orig[i], reps[i]), ]) == 2, na.rm = T)
+	out[pairNames[i], "Union"] <- sum(colSums(dat[c(orig[i], reps[i]), ]) %in% 1:2, na.rm = T)
 	}
   out[, "Not intersection"] <- out[, "Union"] - out[, "Intersection"]
   out[, "Overlap proportion"] <- round(out[, "Intersection"] / out[, "Union"], 3)
