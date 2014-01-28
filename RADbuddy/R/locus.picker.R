f17e05882af1f3823ceeb9f35bd816aac1f7c57a
ahipp@mortonarb.org <ahipp@mortonarb.org@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2014-01-28 15:34:47 +0000

    updating docs and getting rid of one unneeded function

--- RADbuddy/R/locus.picker.R
+++
@@ -1,4 +0,0 @@
-locus.picker <-
-function(pyDat, minThreshold = 3, inds = row.names(pyDat$radSummary$inds.mat)) {
-  out = names(which(colSums(pyDat$radSummary$inds.mat[inds, ]) >= minThreshold))
-  }
