e5bb7a087f1b661f2a049e89f74ea1801cf4b2be
ahipp@mortonarb.org <ahipp@mortonarb.org@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2014-02-11 12:34:15 +0000

    okay, back to default drop.tip; adding a "try" to the drop.tip and unroot to see if we can catch problems here.

--- RADami/R/gen.RAD.loci.datasets.R
+++ RADami/R/gen.RAD.loci.datasets.R
@@ -46,8 +46,16 @@ function(rads, trees = "none", loci = "all", taxa = "all", minTaxa = 4,
 	datFileOut <- paste(fileBase, '.', batch, '/', i, '.phy', sep = '')
 	write.DNAStringSet(locus.list[[i]][locus.taxa], filename = datFileOut)
 	if(trees[1] != 'none') {
-	  trees.out <- lapply(trees, ape::drop.tip, tip = trees[[1]]$tip.label[!trees[[1]]$tip.label %in% locus.taxa])
-	  trees.out <- lapply(trees.out, unroot)
+	  trees.out <- try(lapply(trees, drop.tip, tip = trees[[1]]$tip.label[!trees[[1]]$tip.label %in% locus.taxa]))
+	  if(class(trees.out) == "try-error") {
+	    message('...error with drop.tip -- bailing out...')
+		next
+		}
+	  trees.out <- try(lapply(trees.out, unroot))
+	  if(class(trees.out) == "try-error") {
+	    message('...error with unroot -- bailing out...')
+		next
+		}
 	  class(trees.out) <- 'multiPhylo'
 	  trees.out <- unique(trees.out) # this really slows things down... if there were a way to speed this up it w/b great.
 	  message(paste('... kept', length(trees.out), 'trees'))
