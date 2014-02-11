2edef35303c7c53b1ec19e7f81342f903f0e1703
ahipp@mortonarb.org <ahipp@mortonarb.org@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2014-02-11 08:40:45 +0000

    updating plot.rankedPartitionedRAD to use tree likelihoods from a separate analysis (e.g., full data matrix) as an option

--- trunk/RADami/R/plot.rankedPartitionedRAD.R
+++ trunk/RADami/R/plot.rankedPartitionedRAD.R
@@ -1,15 +1,24 @@
 plot.rankedPartitionedRAD <-
 function(x, 
-                                      fileprefix = NULL,
-									  widthScalar = .85,
-									  panels = c('bestMat', 'worstMat', 'doubleCountMat'),
-									  squareSize = switch(as.character(length(panels)), '2' = 5, '3' = 3),
-									  primeTreeColor = 'red',
-									  primeTreeCharacter = 19,
-									  filebase = 'DEFAULT',
-                                      ...) {
+         tree.lnL.file = NULL,
+		 fileprefix = NULL,
+         widthScalar = .85,
+         panels = c('bestMat', 'worstMat', 'doubleCountMat'),
+         squareSize = switch(as.character(length(panels)), '2' = 5, '3' = 3),
+         primeTreeColor = 'red',
+         primeTreeCharacter = 19,
+         filebase = 'DEFAULT',
+         ...) {
   if(filebase == 'DEFAULT') filebase <- paste(format(Sys.time(), "rad.partitioned.%Y-%m-%d."),  paste(c('minT','rangeL','diffL','noDoubles'), x$params, collapse = "_", sep = ''), '.pdf', sep = '')
   if(class(x) != 'rankedPartitionedRAD') warning('Not the expected object class; this function may misbehave')
+  if(is.null(tree.lnL.file)) {
+    trees.lnL <- colSums(x$radMat)
+	temp.xlab <- 'Tree log-likelihood, full data matrix'
+	}
+  else {
+    trees.lnL <- get.raxml.treeLikelihoods(tree.lnL.file)
+	temp.xlab <- 'Tree log-likelihood, summed over loci'
+	}
   if(!is.null(fileprefix)) pdf(paste(fileprefix, filebase, sep = '.'), width = squareSize*length(panels)*widthScalar, height = squareSize)
   layout(matrix(seq(length(panels)), 1, length(panels)))
   for(i in panels) {
@@ -18,8 +27,8 @@ function(x,
 					   worstMat = 'Number of loci disfavoring tree',
 					   doubleCountMat = 'Loci overlapping, excluded'
 					   )
-	plot(colSums(x$radMat), colSums(x[[i]]), xlab = 'Tree log-likelihood, summed over loci', ylab = temp.ylab, ...)
-    points(colSums(x$radMat)[1], colSums(x[[i]])[1], pch = primeTreeCharacter, col = primeTreeColor)
+	plot(trees.lnL, colSums(x[[i]]), xlab = temp.xlab, ylab = temp.ylab, ...)
+    points(trees.lnL[1], colSums(x[[i]])[1], pch = primeTreeCharacter, col = primeTreeColor)
 	}
   dev.off()
   return('done')
