9686c5ecf89c129c42b649878e33f161d1b0d15c
ahipp@mortonarb.org <ahipp@mortonarb.org@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2014-03-03 10:27:02 +0000

    fixing GEIGER dependency

--- RADami/R/plot.locus.dist.R
+++ RADami/R/plot.locus.dist.R
@@ -13,11 +13,11 @@ function(x, tr, trW = 3, plotW = 5, labelsW = 3, plotGap = 0.25, scalar = 1.5, b
  pch.mat <- matrix(point.pch[1], dim(locD)[1], dim(locD)[2])
  diag(pch.mat) <- point.pch[2]
  nloci <- dim(locD)[1]
- tr.rescaled <- tr
- tree.scalar <- trW / max(heights(tr.rescaled))
- tr.rescaled$edge.length <- tr.rescaled$edge.length * tree.scalar
- #plot(geiger::transform(tr, 'depth', trW), x.lim = c(0, trW + plotW + plotGap * 2 + labelsW), show.tip.label=F, no.margin = T)
- plot(tr.rescaled, x.lim = c(0, trW + plotW + plotGap * 2 + labelsW), show.tip.label=F, no.margin = T)
+ #tr.rescaled <- tr
+ #tree.scalar <- trW / max(heights(tr.rescaled))
+ #tr.rescaled$edge.length <- tr.rescaled$edge.length * tree.scalar
+ plot(rescale(tr, 'depth', trW), x.lim = c(0, trW + plotW + plotGap * 2 + labelsW), show.tip.label=F, no.margin = T)
+ # plot(tr.rescaled, x.lim = c(0, trW + plotW + plotGap * 2 + labelsW), show.tip.label=F, no.margin = T)
  xy <- matrix(seq(nloci), nloci, nloci, byrow = TRUE)
  Xs <- plotW*(as.numeric(t(xy)) / nloci) + trW + plotGap
  points(Xs, as.numeric(xy), pch = as.numeric(pch.mat), cex = as.numeric(locD) * scalar, bg = as.character(color.mat), col = 'black')
@@ -38,7 +38,11 @@ function(x, tr, trW = 3, plotW = 5, labelsW = 3, plotGap = 0.25, scalar = 1.5, b
  legend.text.x <- (length(tr$tip.label) / plotW) * (plotW + plotGap * 3)
  legend.y <- c(trW, trW * 0.6, trW * 0.2)
  # print(c(tr.x.min, tr.x.max))
- plot(geiger::transform(tr, 'depth', trW), x.lim = c(tr.x.min, tr.x.max), direction = 'upwards', show.tip.label=F, no.margin = T)
+ # tr.rescaled <- tr
+ # tree.scalar <- trW / max(heights(tr.rescaled))
+ #tr.rescaled$edge.length <- tr.rescaled$edge.length * tree.scalar
+ plot(rescale(tr, 'depth', trW), x.lim = c(tr.x.min, tr.x.max), direction = 'upwards', show.tip.label=F, no.margin = T)
+ # plot(tr.rescaled, x.lim = c(tr.x.min, tr.x.max), direction = 'upwards', show.tip.label=F, no.margin = T)
  points(rep(legend.dots.x, 2), legend.y[2:3], pch = point.pch[1], bg = cols[1], col = 'black', cex = c(scalar, scalar / 2))
  text(c(legend.header.x, rep(legend.text.x, 2)), legend.y, c('Proportion of total loci', '1.0', '0.5'), cex = c(0.8, 0.6, 0.6), pos = 4)
  out = invisible(list(Xs = Xs[1:length(tr$tip.label)], heights = apply(locD, 2, mean) * barH))
