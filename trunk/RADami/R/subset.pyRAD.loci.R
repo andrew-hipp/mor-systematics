c98cffb347ce8ccf06fdf87f396c80a04e0efac9
ahipp@mortonarb.org <ahipp@mortonarb.org@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2014-03-10 12:58:45 +0000

    possibly adding in a filter for just individuals with certain types of polymorphisms

--- trunk/RADami/R/subset.pyRAD.loci.R
+++ trunk/RADami/R/subset.pyRAD.loci.R
@@ -7,6 +7,10 @@ function(x, loci, taxa, format = 'DNAStringSet', reportInterval = 500, nucVarTyp
 ##  format = only DNAStringSet export supported now
 ##  reportInterval = interval to use for reporting subsetting progress
 ##  nucVarType = either strict for requiring variability to be due to only unambiguous nucleotides (strict, verystrict) or allowing ambiguities to encode variability (relaxed), and insisting on parsimony informativeness (verystrict) or just variability (strict, relaxed) at at least one site
+
+##  TO DO
+##  	add export = nucVarType to specify which individuals get exported
+
   excludedNucs <- switch(nucVarType[1], verystrict = 5:17, strict = 5:17, relaxed = 15:17)
   nucThresh <- switch(nucVarType[1], verystrict = 2, strict = 1, relaxed = 1)
   out <- list(
