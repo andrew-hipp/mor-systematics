284f8c0f514a687d85d81dd6ab834ced57cc5e8f
ahipp@mortonarb.org <ahipp@mortonarb.org@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2014-03-31 13:06:19 +0000

    adding group.subset.pyRAD.loci.R

---
+++ RADami/R/group.subset.pyRAD.loci.R
@@ -0,0 +1,18 @@
+group.subset.pyRAD.loci <- function(dat, groups, mins = 10, loci = names(dat$DNA), use.tidyName = TRUE, ...) {
+# arguments:
+#  dat is a subset.pyRAD.loci object
+#  groups is a list of individuals (can be named) in the groups
+#  
+# value: a matrix with the number of individuals in each group for each locus
+
+  if(use.tidyName) {
+    names(dat$DNA) <- tidyName(names(dat$DNA), ...)
+	groups <- lapply(groups, tidyName, ...)
+	}
+  inds.mats <- sapply(dat$DNA, function(x) c(sapply(groups, function(y) sum(names(x) %in% y)), total = length(x)))
+  if(!is.na(mins[1])) leave.in <- apply(out, 1, function(x) all(x >= mins))
+  else leave.in <- rep(TRUE, dim(out)[1])
+  if(!is.na(loci[1])) out <- out[intersect(row.names(out), loci), ]
+  out
+  }
+ 
