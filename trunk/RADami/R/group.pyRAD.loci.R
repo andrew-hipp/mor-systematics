ca8f6ffc0e7b621546fcc971c1343d789d26578d
ahipp@mortonarb.org <ahipp@mortonarb.org@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2014-04-01 09:35:01 +0000

    multicore support for group.pyRAD.loci.R

--- trunk/RADami/R/group.pyRAD.loci.R
+++ trunk/RADami/R/group.pyRAD.loci.R
@@ -1,4 +1,4 @@
-group.pyRAD.loci <- function(dat, groups, mins = 10, loci = dimnames(dat$radSummary$inds.mat)[[2]], use.tidyName = TRUE, ...) {
+group.pyRAD.loci <- function(dat, groups, mins = 10, loci = dimnames(dat$radSummary$inds.mat)[[2]], use.tidyName = TRUE, cores = 8, ...) {
 # arguments:
 #  dat is a pyRAD.loci object
 #  groups is a list of individuals (can be named) in the groups
@@ -9,8 +9,8 @@ group.pyRAD.loci <- function(dat, groups, mins = 10, loci = dimnames(dat$radSumm
     row.names(dat$radSummary$inds.mat) <- tidyName(row.names(dat$radSummary$inds.mat), ...)
 	groups <- lapply(groups, tidyName, ...)
 	}
-  inds.mats <- lapply(groups, function(x) temp <- dat$radSummary$inds.mat[x[x %in% row.names(dat$radSummary$inds.mat)], ])
-  out <- sapply(inds.mats, function(x) apply(x, 2, sum))
+  inds.mats <- mclapply(groups, function(x) temp <- dat$radSummary$inds.mat[x[x %in% row.names(dat$radSummary$inds.mat)], ], mc.cores = cores)
+  out <- mcmapply(function(x) apply(x, 2, sum), inds.mats, mc.cores = cores)
   if(!is.na(mins[1])) leave.in <- apply(out, 1, function(x) all(x >= mins))
   else leave.in <- rep(TRUE, dim(out)[1])
   out <- cbind(out[leave.in, ], total = colSums(dat$radSummary$inds.mat)[leave.in])
