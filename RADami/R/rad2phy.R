ae19f7d3249624a24c6373da976fc64d97faee19
ahipp@mortonarb.org <ahipp@mortonarb.org@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2014-08-07 16:00:07 +0000

    adding ogfile to rad2phy

--- RADami/R/rad2phy.R
+++ RADami/R/rad2phy.R
@@ -1,7 +1,8 @@
 rad2phy <-
-function(pyDat, inds = row.names(pyDat), loci = dimnames(pyDat)[[2]], outfile = 'pyMat.out.phy', padding = 50, verbose = FALSE) {
+function(pyDat, inds = row.names(pyDat), loci = dimnames(pyDat)[[2]], outfile = 'pyMat.out.phy', padding = 50, verbose = FALSE, logfile = 'rad2phy.log') {
 ## makes a phylip-style data matrix from rad.mat output, limiting by individuals and loci
   if(class(pyDat) != "rad.mat") warning("I'm expecting output from rad.mat")
+  outfile.name = outfile
   outfile = file(outfile, "wt")
   open(outfile)
   cat(paste(length(inds), sum(sapply(pyDat[inds[1], loci], nchar)), "\n"), file = outfile) #header: number of individuals, number of bases
@@ -13,4 +14,11 @@ function(pyDat, inds = row.names(pyDat), loci = dimnames(pyDat)[[2]], outfile = 
 	cat("\n", file = outfile) # endline
 	}
   close(outfile)
+  if(!is.na(logfile) & logfile != '') logfile = file(logfile, 'wt')
+  open(logfile)
+  writeLines(timestamp(), file = logfile)
+  writeLines(paste("Filename:", outfile.name), file = logfile)
+  writeLines("Loci included in phylip file:", file = logfile)
+  writeLines(paste("\t", loci, sep = ''), file = logfile)
+  close(logfile)
   }
