183b8e59db1e693a547fd13c0372494eab6659f0
ahipp@mortonarb.org <ahipp@mortonarb.org@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2014-11-03 18:12:37 +0000

    adding fasta.name.cleaner.R to scrub fasta names

---
+++ cariceae/R/fasta.name.cleaner.R
@@ -0,0 +1,23 @@
+fasta.name.scrubber <- function(x, delim = '_', elements = 3, ...) {
+## just scrubs names down to the extraction code, peels off scientific names
+## x is a character vector
+
+  x.list <- strsplit(x, delim, ...)
+  x.out <- sapply(x.list, function(y) {paste(y[(length(y) - 2):length(y)], collapse = delim)}, USE.NAMES = FALSE)
+  x.out
+  }
+
+clean.all.names <- function(directory = './SEQS/', pattern = '.fas', ...) {
+  newdir <- paste(directory, 'new.seqs.', format(Sys.time(), "%Y-%m-%d"), sep = '')
+  dir.create(newdir)
+  all.dat <- lapply(dir(directory, patt = pattern, full = T), read.dna, format = 'fasta')
+  names(all.dat) <- gsub(pattern, '', dir(directory, patt = pattern))
+  for(i in names(all.dat)) {
+    out.table <- cbind(old.name = row.names(all.dat[[i]]), 
+	                   new.name = fasta.name.scrubber(row.names(all.dat[[i]]), ...))
+	row.names(all.dat[[i]]) <- out.table[, 'new.name']
+	write.csv(out.table, paste(newdir, '/', i, '.nameTable.csv', sep = ''))
+	write.dna(all.dat[[i]], paste(newdir, '/', i, '.renamed.fas', sep = ''), format = 'fasta')
+	}
+  }
+  
