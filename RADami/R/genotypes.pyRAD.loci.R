1e583913ecf62de1eef06ff46158cdb8b2e56a0c
ahipp@mortonarb.org <ahipp@mortonarb.org@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2014-12-18 22:06:47 +0000

    still fixing genotypes.pyRAD.loci

--- RADami/R/genotypes.pyRAD.loci.R
+++ RADami/R/genotypes.pyRAD.loci.R
@@ -32,7 +32,7 @@ genotypes.pyRAD.loci <- function(dat, groups, loci = 'all', taxa = 'all',
         y.mat <- as.matrix(y)
         y.mat.uns <- apply(y.mat, 2, function(x) length(unique(x)) > 1)
         y.mat <- y.mat[, y.mat.uns]
-        if(dim(y.mat)[2]) == 0) return('failed')	
+        if(dim(y.mat)[2] == 0) return('failed')	
         trans.dna <- t(apply(as.matrix(y), 1, function(x) IUPAC_CODE_MAP[x]))
 	trans.dna <- t(apply(trans.dna, 1, function(x) gsub('A', '1', x)))
 	trans.dna <- t(apply(trans.dna, 1, function(x) gsub('C', '2', x)))
