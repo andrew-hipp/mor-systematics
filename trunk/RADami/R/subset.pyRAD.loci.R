3bc144de898323651fcb4975d18681ae42fdf646
ahipp@mortonarb.org <ahipp@mortonarb.org@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2014-12-18 20:42:18 +0000

    get rid of unneeded single-core branch

--- trunk/RADami/R/subset.pyRAD.loci.R
+++ trunk/RADami/R/subset.pyRAD.loci.R
@@ -16,67 +16,31 @@ function(x, loci = colnames(x$radSummary$inds.mat), taxa = row.names(x$radSummar
   nucThresh <- switch(nucVarType[1], verystrict = 2, strict = 1, relaxed = 1)
   if(use.tidyName) inds.vector <- tidyName(x$tips, ...) %in% tidyName(taxa, ...)
   else inds.vector <- x$tips %in% taxa
-  counter = 0
-  start.time <- Sys.time()
-  if(cores == -1) {
-  # note that this is added because I would like to get rid of the redundancy; no reason to code two different ways
-  out <- list(
-    DNA = structure(vector('list', length(loci)), names = loci),
-    variable = structure(logical(length(loci)), names = loci),
-	ntaxa = structure(integer(length(loci)), names = loci),
-	snpLocs = structure(vector('list', length(loci)), names = loci)
-	)
-    for(i in loci) {
-	  counter <- counter + 1
-	  if(counter / reportInterval - counter %/% reportInterval == 0) {
-  	    message(paste('... subsetting', counter, 'of', length(loci), 
- 	    '-- Estimated time remaining =', round(((Sys.time() - start.time) / counter) * (length(loci) - counter), 1), attr(Sys.time() - start.time, 'units')
-  	    ))
-	  }
-      seq.index <- x$locus.index == i & inds.vector
-	  out$DNA[[i]] <- DNAStringSet(x$seqs[seq.index])
-	  names(out$DNA[[i]]) <- x$tips[seq.index]
-	  out$snpLocs[[i]] <- which(apply(consensusMatrix(out$DNA[[i]])[-c(excludedNucs), ], 2, function(x) sum(x >= nucThresh) > 1))
-	  out$variable[i] <- length(out$snpLocs[[i]] > 0)
-	  out$ntaxa[i] <- sum(seq.index)
-	  if(snpsOnly) {
-	    tempDNA <- as.matrix(as.matrix(out$DNA[[i]])[, out$snpLocs[[i]]])
-	    out$DNA[[i]] <- try(DNAStringSet(apply(tempDNA, 1, paste, collapse = '')))
-	    if(class(out$DNA[[i]]) == 'try-error') {
-	      message(paste('failed on locus', i))
-		  out$DNA[[i]] <- NULL
-		  }
-	    } # close if
-	  } # close i
-	} # close cores = 1 branch
-  ## delete down to here once I figure out whether the single branch works for both cores and multicores
-  else {
-    do.it.dna <- function(i) {
+  do.it.dna <- function(i) {
 	  seq.index <- x$locus.index == i & inds.vector
 	  dna.temp <- DNAStringSet(x$seqs[seq.index])
 	  names(dna.temp) <- x$tips[seq.index]
 	  return(dna.temp)
 	  } # close do.it.dna
-	if(cores != 1) out <- list(DNA = mclapply(loci, do.it.dna, mc.cores = cores))
-	else out <- list(DNA = lapply(loci, do.it.dna))
-	names(out$DNA) <- loci
-	if(cores != 1) out$snpLocs <- mclapply(out$DNA, function(i) try(which(apply(consensusMatrix(i)[-c(excludedNucs), ], 2, function(x) sum(x >= nucThresh) > 1))), mc.cores = cores)
-	else out$snpLocs <- lapply(out$DNA, function(i) try(which(apply(consensusMatrix(i)[-c(excludedNucs), ], 2, function(x) sum(x >= nucThresh) > 1))))
-	if(snpsOnly) {
-	  do.it.snps <- function(i) {
+  if(cores != 1) out <- list(DNA = mclapply(loci, do.it.dna, mc.cores = cores))
+  else out <- list(DNA = lapply(loci, do.it.dna))
+  names(out$DNA) <- loci
+  if(cores != 1) out$snpLocs <- mclapply(out$DNA, function(i) try(which(apply(consensusMatrix(i)[-c(excludedNucs), ], 2, function(x) sum(x >= nucThresh) > 1))), mc.cores = cores)
+  else out$snpLocs <- lapply(out$DNA, function(i) try(which(apply(consensusMatrix(i)[-c(excludedNucs), ], 2, function(x) sum(x >= nucThresh) > 1))))
+  if(snpsOnly) {
+	do.it.snps <- function(i) {
 	    tempDNA <- as.matrix(as.matrix(out$DNA[[i]])[, out$snpLocs[[i]]])
 		snps.out <- try(DNAStringSet(apply(tempDNA, 1, paste, collapse = '')))
 		if(class(snps.out) == 'try-error') message(paste('failed on locus', i))
 		return(snps.out)
 	    } # close do.it.snps
-	  if(cores != 1) out$DNA <- mclapply(loci, do.it.snps, mc.cores = cores)
-	  else out$DNA <- lapply(loci, do.it.snps)
-	  names(out$DNA) <- loci
-	  out$DNA <- out$DNA[sapply(out$DNA, class) != 'try-error']
-	  } # close if snpsOnly
-	out$ntaxa <- sapply(out$DNA, length)
-	out$variable <- sapply(out$snpLocs, function(i) length(i) > 0)
-	} # close else -- the multicore branch
+    if(cores != 1) out$DNA <- mclapply(loci, do.it.snps, mc.cores = cores)
+    else out$DNA <- lapply(loci, do.it.snps)
+    names(out$DNA) <- loci
+    out$DNA <- out$DNA[sapply(out$DNA, class) != 'try-error']
+    } # close if snpsOnly
+  out$ntaxa <- sapply(out$DNA, length)
+  out$variable <- sapply(out$snpLocs, function(i) length(i) > 0)
   class(out) <- 'subset.pyRAD.loci'
   return(out)
   }
