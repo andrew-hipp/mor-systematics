5e34ee969ae84b8014cd32caeb1c24a773554fb4
ahipp@mortonarb.org <ahipp@mortonarb.org@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2014-12-18 20:59:55 +0000

    adding tidyName option to filter.by.R

--- RADami/R/filter.by.R
+++ RADami/R/filter.by.R
@@ -1,9 +1,13 @@
-filter.by <- function(dat, taxa, threshold = 'all') {
+filter.by <- function(dat, taxa, threshold = 'all', use.tidyName = FALSE, ...) {
   ## returns just loci for which the requested taxa are present at some threshold
   ## default to returning 'all'
   if(!class(dat) %in% c('summary.pyRAD.loci', 'pyRAD.loci')) stop("This function only works with summary.pyRAD.loci datatypes")
-  if(class(dat) == 'pyRAD.loci') dat.mat <- dat$radSummary$inds.mat[taxa, ]
-    else dat.mat <- dat$inds.mat[taxa, ] # this is the case if you pass in a summary.pyRAD.loci object
+  if(class(dat) == 'pyRAD.loci') dat <- dat$radSummary$inds.mat
+  if(use.tidyName) {
+    row.names(dat) <- tidyName(row.names(dat), ...)
+	taxa <- tidyName(taxa, ...)
+	}
+  dat.mat <- dat$inds.mat[taxa, ] # this is the case if you pass in a summary.pyRAD.loci object
   if(threshold == 'all') threshold <- length(taxa)
   return(names(which(apply(dat.mat, 2, sum) >= threshold)))
   }
