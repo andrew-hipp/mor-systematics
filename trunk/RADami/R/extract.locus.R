023ba14c54ce91468d4196aa0d25460c4084707c
ahipp@mortonarb.org <ahipp@mortonarb.org@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2015-01-07 17:27:15 +0000

    creating an extract.locus.R function

--- trunk/RADami/R/extract.locus.R
+++ trunk/RADami/R/extract.locus.R
@@ -1,2 +1,5 @@
-extract.locus <- function(pyDat, locName, format = c('text', 'fasta', 'DNAStringSet')) {
+extract.locus <- function(pyDat, locName, dat.format = c('text', 'fasta', 'DNAStringSet')) {
+  seqs <- pyDat$seqs[pyDat$locus.index == locName]
+  if(dat.format[1] == 'text') names(seqs) <- pyDat$tips[pyDat$locus.index == locName]
+  as.matrix(seqs)
 }
