f171866bd6d2a13bcb078b6df30ad85d147cc8b0
ahipp@mortonarb.org <ahipp@mortonarb.org@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2015-01-09 20:50:29 +0000

    cleaning up rad.sharing.R to work with three-taxon statements

--- trunk/RADami/R/rad.sharing.R
+++ trunk/RADami/R/rad.sharing.R
@@ -6,27 +6,37 @@ function(taxa, radMat) {
   else v1 <- radMat[taxa[[1]], ]
   if(length(taxa[[2]]) > 1) v2 <- apply(radMat[taxa[[2]], ], 2, any)
   else v2 <- radMat[taxa[[2]], ]
-  v.out <- v1 & v2
-  out <- list(abs = sum(v.out), prop = sum(v.out) / sum(v1 | v2))
+  if(length(taxa) == 3) {
+    if(length(taxa[[3]]) > 1) v3 <- apply(radMat[taxa[[3]], ], 2, any)
+    else v3 <- radMat[taxa[[2]], ]
+	v.mat <- do.call(rbind, list(v1, v2, v3))
+    }
+  else v.mat <- rbind(v1, v2)
+  v.out <- sum(apply(v.mat, 2, all))
+  v.denom <- sum(apply(v.mat, 2, any))
+  out <- c(abs = v.out, prop = v.out / v.denom)
   return(out)
 }
 
 do.all.nodes <-
 function(tr, rads) {
   to.do <- unique(tr$edge[, 1])
-  out <- matrix(sapply(to.do, function(x) compare.loci(node.tips(tr, x), rads)), dimnames = list(to.do, 'sharedLoci'))
+  out <- t(sapply(to.do, function(x) compare.loci(node.tips(tr, x), rads)))
+  row.names(out) <- to.do
   out 
 }
 
 node.tips <-
-function(tr, node) {
+function(tr, node, getOut = c('desc.og', 'desc'), add.og = '>AC_h') {
   to.do <- tr$edge[which(tr$edge[,1] == node), 2]
   out <- lapply(Descendants(tr, to.do, 'tips'), function(x) tr$tip.label[x])
+  if(getOut[1]== 'desc.og') out[[3]] <- c(tr$tip.label[!tr$tip.label %in% unique(unlist(out))], add.og)
   return(out)
 }
 
 do.a.rad.comparison.tree <- function(tr, rads, cex.scalar = 2, ...) {
-  node.comparisons <- do.all.nodes(tr, rads)
+  nodes.comparison <- do.all.nodes(tr, rads)
   plot(tr)
-  nodelabels(pie = nodes.comparisons$prop[,1], node = as.numeric(row.names(nodes.comparison$prop)), cex = cex.scalar * nodes.comparison$abs[, 1]/max(nodes.comparison$abs[, 1]), piecol = c('black', 'white'), ...)
+  nodelabels(pie = nodes.comparison[,'prop'], node = as.numeric(row.names(nodes.comparison)), cex = cex.scalar * nodes.comparison[, 'abs']/max(nodes.comparison[, 'abs']), piecol = c('black', 'white'), ...)
+  return(nodes.comparison)
   }
