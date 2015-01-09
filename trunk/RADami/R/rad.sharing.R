dc5ac348fc1fb0dd00e54d97d0ad69d51f66263e
ahipp@mortonarb.org <ahipp@mortonarb.org@bb71a78d-21bc-3f06-f638-4f78a0d2836f>
2015-01-09 19:50:12 +0000

    updating rad.sharing.R to make it easier to plot a sharing tree

--- trunk/RADami/R/rad.sharing.R
+++ trunk/RADami/R/rad.sharing.R
@@ -1,5 +1,5 @@
 compare.loci <-
-function(taxa, radMat, give = c('abs', 'prop')) {
+function(taxa, radMat) {
   ## taxa is a pair of taxon vectors
   ## radMat is an inds.mat
   if(length(taxa[[1]]) > 1) v1 <- apply(radMat[taxa[[1]], ], 2, any)
@@ -7,19 +7,26 @@ function(taxa, radMat, give = c('abs', 'prop')) {
   if(length(taxa[[2]]) > 1) v2 <- apply(radMat[taxa[[2]], ], 2, any)
   else v2 <- radMat[taxa[[2]], ]
   v.out <- v1 & v2
-  if(give[1] == 'abs') out <- sum(v.out)
-  if(give[1] == 'prop') out <- sum(v.out) / sum(v1 | v2)
+  out <- list(abs = sum(v.out), prop = sum(v.out) / sum(v1 | v2))
   return(out)
 }
+
 do.all.nodes <-
-function(tr, rads, ...) {
+function(tr, rads) {
   to.do <- unique(tr$edge[, 1])
-  out <- matrix(sapply(to.do, function(x) compare.loci(node.tips(tr, x), rads, ...)), dimnames = list(to.do, 'sharedLoci'))
+  out <- matrix(sapply(to.do, function(x) compare.loci(node.tips(tr, x), rads)), dimnames = list(to.do, 'sharedLoci'))
   out 
 }
+
 node.tips <-
 function(tr, node) {
   to.do <- tr$edge[which(tr$edge[,1] == node), 2]
   out <- lapply(Descendants(tr, to.do, 'tips'), function(x) tr$tip.label[x])
   return(out)
 }
+
+do.a.rad.comparison.tree <- function(tr, rads, cex.scalar = 2, ...) {
+  node.comparisons <- do.all.nodes(tr, rads)
+  plot(tr)
+  nodelabels(pie = nodes.comparisons$prop[,1], node = as.numeric(row.names(nodes.comparison$prop)), cex = cex.scalar * nodes.comparison$abs[, 1]/max(nodes.comparison$abs[, 1]), piecol = c('black', 'white'), ...)
+  }
