6aafe85b4dbbcf547ede1cdb6176df2c309127dd
andrew-hipp <andrew.hipp@gmail.com>
2015-12-01 06:21:03 -0600

    Cleaning up returned objects in make.gene.matrix and plot.geneMat

--- phyloBuddy/R/plot.geneMat.R
+++ phyloBuddy/R/plot.geneMat.R
@@ -15,7 +15,7 @@ plot.geneMat <- function(x, tr = NA, genes = colnames(x)[6:(dim(x)[2])], panes =
   nGenes <- dim(x)[1]
   col.genes <- rep(geneColors, nGenes/length(geneColors))
   par(mai=margins)
-  plot(1, xlim = c(1, nInds), ylim = c(1, nGenes), type = 'n', xlab = '', ylab = '', axes = F)
+  out.ngenes <- plot(1, xlim = c(1, nInds), ylim = c(1, nGenes), type = 'n', xlab = '', ylab = '', axes = F)
   axis(2, at = seq(from = 1, to = nGenes-1, by = 2), labels = row.names(x)[seq(from = 1, to = nGenes-1, by = 2)], las = 2, cex.axis = label.cex, col.axis = col.genes[1])
   axis(2, at = seq(from = 2, to = nGenes, by = 2), labels = row.names(x)[seq(from = 2, to = nGenes, by = 2)], las = 2, cex.axis = label.cex, col.axis = col.genes[2])
 
@@ -26,11 +26,12 @@ plot.geneMat <- function(x, tr = NA, genes = colnames(x)[6:(dim(x)[2])], panes =
 	}
   if(!is.na(tr[1])) {
     geneSums <- apply(x, 2, function(y) sum(y != ''))
-	plot(geneSums, type = 'p', pch = 19, axes = F, ylab = 'Genes sampled', cex.lab = 0.8)
+	out.geneSums <- plot(geneSums, type = 'p', pch = 19, axes = F, ylab = 'Genes sampled', cex.lab = 0.8)
 	abline(h = mean(geneSums), lty = 'dashed')
 	axis(2, at = seq(geneSums), labels = seq(geneSums), las = 2)
-	plot(tr, direction = 'upwards', show.tip.label = F, no.margin = F)
+	out.tree <- plot(tr, direction = 'upwards', show.tip.label = F, no.margin = F)
     }
   if(!is.na(pdfTitle)) dev.off()
-  invisible(0) # should return something more useful here
+  out <- list(tree = out.tree, geneSums = out.geneSums, ngenes = out.ngenes)
+  invisible(out) # should return something more useful here
   }
