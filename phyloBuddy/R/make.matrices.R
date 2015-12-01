92ef7d5bd9773ebbd956aa743a78ecd0309d9d3f
andrew-hipp <andrew.hipp@gmail.com>
2015-12-01 05:51:02 -0600

    make.unique.vouchers cleanup
    extraneous comma removed

--- phyloBuddy/R/make.matrices.R
+++ phyloBuddy/R/make.matrices.R
@@ -92,7 +92,7 @@ refine.fasta <- function(basedir = choose.dir()) {
   return('done!')
   }
 
-make.unique.vouchers <- function(metadata, , voucherFormula = c("Primary.collector.last.name", "Collector.number", "isolate", "CollectionNumber", "Collection")) {
+make.unique.vouchers <- function(metadata, voucherFormula = c("Primary.collector.last.name", "Collector.number", "isolate", "CollectionNumber", "Collection")) {
       vouchers.ln.cn <- apply(metadata[voucherFormula], 1, function(x) paste(tidyName(x[!is.na(x)]), collapse = ''))
       names(vouchers.ln.cn) <- metadata$NCBI_voucher # note that NCBI_accession is not actually the accession number used in NCBI! The preferred accession number is primary_accession
       return(vouchers.ln.cn)
