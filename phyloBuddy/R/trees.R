0e92a190d88452f4b57c20d751d52b32c25c96ca
andrew-hipp <andrew.hipp@gmail.com>
2015-12-03 16:21:25 -0600

    Refining taxonomic disparity
    summary.by.elements now returns a matrix with the disparity index as
    well as a list for all taxa > 2 tips (by default), in which the effect
    of each taxon on total tree length is reported. The most egregious
    outliers are expected of course to have the largest effects on total
    tree length when they are deleted.

--- phyloBuddy/R/trees.R
+++ phyloBuddy/R/trees.R
@@ -50,14 +50,26 @@ tips.expected <- function(tr, tips, value = FALSE, ...) {
   out
   }
 
-summary.by.elements <- function(tr, ...) {
+summary.by.elements <- function(tr, returnEffectOnTreeLength = TRUE, minSizeForEffect = 3, recommendDrops = TRUE, ...) {
 ## this is the taxonomic disparity function
   all.elements <- label.elements(tr, ...)
   unique.elements <- sort(unique(all.elements))
   out <- cbind(count = sapply(unique.elements, function(x) sum(all.elements == x)),
                expected = sapply(unique.elements, function(x) tips.expected(tr, names(all.elements)[all.elements == x]))
 			   )
-  out <- cbind(out, disparity = out[, 'expected'] - out[, 'count'])
+  out <- list(disparity.mat = cbind(out, disparity = out[, 'expected'] - out[, 'count']), effectSize = NA)
+  if(returnEffectOnTreeLength) {
+    message('calculating effect size... please be patient')
+	effectOnTreeLength <- lapply(unique.elements, function(x) {
+	  if(out$disparity.mat[x, 'count'] < minSizeForEffect) return(0)
+	  message(paste('doing effect size for', x))
+	  tr.temp <- drop.tip(tr, tr$tip.label[which(all.elements != x)])
+	  effectSizeByTaxon <- sum(tr.temp$edge.length) - sapply(tr.temp$tip.label, function(y) sum(drop.tip(tr.temp, y)$edge.length))
+	  return(effectSizeByTaxon)
+	  })
+	out$effectSize <- effectOnTreeLength
+	names(out$effectSize) <- unique.elements
+	}
   out
   }
 
