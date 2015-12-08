1dec1de54097dadeba424ce0f61e1205afada4f2
andrew-hipp <andrew.hipp@gmail.com>
2015-12-08 05:28:27 -0600

    label.elements checks for class of x
    now the class of x is checked rather than having a flag for trees

--- phyloBuddy/R/trees.R
+++ phyloBuddy/R/trees.R
@@ -19,9 +19,10 @@ summarize.gene.coverage <- function(datMat = cariceae.concat.4genes.summary, ext
   }
 
 ## tree traversal and counting
-label.elements <- function(tr, delim = '|', returnNum = 1, returnDelim = ' ', isTree = TRUE, ...) {
+label.elements <- function(x, delim = '|', returnNum = 1, returnDelim = ' ', ...) {
 ## finds any label at the tips; default assumes pipe delimitation, and the element of interest is b/f the first pipe
-  if(isTree) labelVector <- tr$tip.label
+  if('phylo' %in% class(x)) labelVector <- x$tip.label
+  else(labelVector) <- x
   out <- sapply(labelVector, function(x) paste(strsplit(x, delim, ...)[[1]][returnNum], collapse = returnDelim))
   out
   }
