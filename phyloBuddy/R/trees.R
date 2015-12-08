76643997fd623ae5da12344fd0433dddbc0fd54f
andrew-hipp <andrew.hipp@gmail.com>
2015-12-08 05:30:26 -0600

    label.elements
    fixing a parenthesis error

--- phyloBuddy/R/trees.R
+++ phyloBuddy/R/trees.R
@@ -22,7 +22,7 @@ summarize.gene.coverage <- function(datMat = cariceae.concat.4genes.summary, ext
 label.elements <- function(x, delim = '|', returnNum = 1, returnDelim = ' ', ...) {
 ## finds any label at the tips; default assumes pipe delimitation, and the element of interest is b/f the first pipe
   if('phylo' %in% class(x)) labelVector <- x$tip.label
-  else(labelVector) <- x
+  else labelVector <- x
   out <- sapply(labelVector, function(x) paste(strsplit(x, delim, ...)[[1]][returnNum], collapse = returnDelim))
   out
   }
