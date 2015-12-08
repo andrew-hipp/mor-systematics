fe3eea67c5a6923f0b52c13ac4fcd61b7234a63c
andrew-hipp <andrew.hipp@gmail.com>
2015-12-08 05:25:01 -0600

    label.elements works for any vector now, if you want to pass in a non-tree vector, you can do so; added isTree argument

--- phyloBuddy/R/trees.R
+++ phyloBuddy/R/trees.R
@@ -19,9 +19,10 @@ summarize.gene.coverage <- function(datMat = cariceae.concat.4genes.summary, ext
   }
 
 ## tree traversal and counting
-label.elements <- function(tree, delim = '|', returnNum = 1, returnDelim = ' ', ...) {
+label.elements <- function(tr, delim = '|', returnNum = 1, returnDelim = ' ', isTree = TRUE, ...) {
 ## finds any label at the tips; default assumes pipe delimitation, and the element of interest is b/f the first pipe
-  out <- sapply(tree$tip.label, function(x) paste(strsplit(x, delim, ...)[[1]][returnNum], collapse = returnDelim))
+  if(isTree) labelVector <- tr$tip.label
+  out <- sapply(labelVector, function(x) paste(strsplit(x, delim, ...)[[1]][returnNum], collapse = returnDelim))
   out
   }
 
