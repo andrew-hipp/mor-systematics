81f968de669c30a44d9f0b23192a188454203e6c
andrew-hipp <andrew.hipp@gmail.com>
2015-12-11 00:45:54 -0600

    Adding GCG NCBI files
    A set of zipped directories for NCBI Carex paper review

---
+++ cariceae/ncbi.2015/data/SCRIPTS/raxFinal.2015.12.03.sh
@@ -0,0 +1,6 @@
+raxmlHPC-PTHREADS-AVX -f a -T 8 -m GTRCAT -x 12345 -p 12345 -# 100 -s ../MATRICES/concat.20151129.firstFive.fas -n concat.20151129.firstFive.tre
+raxmlHPC-PTHREADS-AVX -f a -T 8 -m GTRCAT -x 12345 -p 12345 -# 100 -s ../MATRICES/concat.20151129.fas -n concat.20151129.tre
+raxmlHPC-PTHREADS-AVX -f a -T 8 -m GTRCAT -x 12345 -p 12345 -# 100 -s ../MATRICES/ITS_scaffold.fas -n ITS_scaffold.tre
+raxmlHPC-PTHREADS-AVX -f a -T 8 -m GTRCAT -x 12345 -p 12345 -# 100 -s ../MATRICES/ITS_ETS.fas -n ITS_ETS.tre
+raxmlHPC-PTHREADS-AVX -f a -T 8 -m GTRCAT -x 12345 -p 12345 -# 100 -s ../MATRICES/ETS_only.fas -n ETS_only.tre
+raxmlHPC-PTHREADS-AVX -f a -T 8 -m GTRCAT -x 12345 -p 12345 -# 100 -s ../MATRICES/ITS_only.edit.fas -n ITS_only.tre
