53d3985d4fba7f168212f6921b9c6cbed8f78582
andrew-hipp <andrew.hipp@gmail.com>
2020-11-19 13:32:23 -0600

    Updating with outfile name

--- AAA.Lab/RAD.mover.R
+++ AAA.Lab/RAD.mover.R
@@ -4,18 +4,33 @@
 library(magrittr)
 
 dat.metaFile = 'https://www.dropbox.com/s/81ppkw8xqycmxz7/RADSpecimensFloragenexLiveCopy_2020-11-16.csv?raw=1'
-dat.dir = '.'
+dat.dir = './'
 seqPatt = 'fastq.gz'
 stripPost = '_R1'
 fileDrops = 'FGXCONTROL'
 matchCol = 'Extraction_Tube_no'
+outPath = '/mnt/LAB_DATA/RAD-data/'
+outPrefix = 'AAA.'
+
 dat.meta <- read.csv(dat.metaFile)
-dat.files <- dir(dat.dir, patt = seqPatt)
-if(!is.null(stripPost)) {
-  dat.files <- sapply(strsplit(dat.files, stripPost, fixed = T), '[', 1)
-}
-dat.files <- grep('FGXCONTROL', dat.files, invert = T, value = T)
+dat.filesOrig <- dir(dat.dir, patt = seqPatt)
+dat.filesOrig <- grep('FGXCONTROL', dat.filesOrig, invert = T, value = T)
+dat.files <- sapply(strsplit(dat.filesOrig, stripPost, fixed = T), '[', 1)
 
 dat.files.row <-
   sapply(dat.files, grep, x = dat.meta[[matchCol]])
 if(class(dat.files.row) != 'integer') stop('Check errors: dat.files.row')
+
+dat.files.species <- dat.meta$Species[dat.files.row] %>% as.character
+dat.files.genus <-
+  dat.files.species %>%
+  as.character %>%
+  strsplit(split = ' ', fixed = T) %>%
+  sapply(FUN = '[', 1) %>%
+  toupper
+
+out <- paste(
+  'mv ',
+  dat.dir, dat.filesOrig, ' ',
+  out.path, outPrefix,
+  sep = '')
