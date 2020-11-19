c4a62dba9867aed963101685b29b89c360b2e98c
andrew-hipp <andrew.hipp@gmail.com>
2020-11-19 12:10:03 -0600

    Read in file names and data file

--- AAA.Lab/RAD.mover.R
+++ AAA.Lab/RAD.mover.R
@@ -1,2 +1,12 @@
 ## move RAD-seq data after demultiplexing
+## assumes you start in the folder where the fastq are
+dat.metaFile = 'https://www.dropbox.com/s/81ppkw8xqycmxz7/RADSpecimensFloragenexLiveCopy_2020-11-16.csv?raw=1'
+dat.dir = '.'
+seqPatt = 'fastq.gz'
+stripPost = '_R1'
 
+dat.meta <- read.csv(datFile)
+dat.files <- dir(dat.dir, patt = seqPatt)
+if(!is.null(stripPost)) {
+  dat.files <- sapply(strsplit(dat.files, stripPost, fixed = T), '[', 1)
+}
