a9700e5a167278115b32bc4083110de9ef060522
andrew-hipp <andrew.hipp@gmail.com>
2020-12-07 09:35:20 -0600

    Reading metadata

--- AAA.Lab/RAD.renamer.R
+++ AAA.Lab/RAD.renamer.R
@@ -1,1 +1,8 @@
 ## rename RAD-seq trees after you run them
+
+dat.metaFile = 'https://raw.githubusercontent.com/andrew-hipp/mor-systematics/master/AAA.DATA/RADSpecimensFloragenexLiveCopy_2020-11-16.csv'
+if(!exists(treeToDo)) {
+  stop('Assign the tree you want to relabel to treeToDo and rerun')
+  }
+
+dat.meta <- read.csv(dat.metaFile)
