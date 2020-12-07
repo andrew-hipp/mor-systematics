a7a8563e433e12395226b3b4abb7d1f095aecba1
andrew-hipp <andrew.hipp@gmail.com>
2020-12-07 09:36:17 -0600

    making treeToDo string

--- AAA.Lab/RAD.renamer.R
+++ AAA.Lab/RAD.renamer.R
@@ -1,7 +1,7 @@
 ## rename RAD-seq trees after you run them
 
 dat.metaFile = 'https://raw.githubusercontent.com/andrew-hipp/mor-systematics/master/AAA.DATA/RADSpecimensFloragenexLiveCopy_2020-11-16.csv'
-if(!exists(treeToDo)) {
+if(!exists('treeToDo')) {
   stop('Assign the tree you want to relabel to treeToDo and rerun')
   }
 
