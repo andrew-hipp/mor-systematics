5f62c753263b422ce51d78a2c387fb702fd22575
andrew-hipp <andrew.hipp@gmail.com>
2021-01-28 20:54:03 -0600

    Updated RAD.renamer to function

--- AAA.LAB/RAD.renamer.R
+++ AAA.LAB/RAD.renamer.R
@@ -1,36 +1,45 @@
 ## rename RAD-seq trees after you run them
 
-library(magrittr)
-library(ape)
+require(magrittr)
+require(ape)
 
-dat.metaFile = 'https://raw.githubusercontent.com/andrew-hipp/mor-systematics/master/AAA.DATA/RADSpecimensFloragenexLiveCopy_2020-11-16.csv'
-matchCol = 'Extraction_Tube_no'
-labelCols = c(
-  "Species",
-  "Inst.",
-  "Extraction_Tube_no"
-  )
-delim = '|'
-  # "Collector_no", "Wild_Garden_Collected",
-  # "Original_Source_Information", "Vouchers", "MOR_Herbarium_Accession_Number",
-  # "Notes", "Voucher_Image", "Photo_1", "Photo_2", "City", "County",
-  # "State_Province", "Country", "Lat", "Long", "Georeference_Source",
-  # "Cronn_CP", "AFLP", "Section", "Floragenex.Library.Name", "SEQUENCED.ON",
-  # "SEQUENCING.DETAILS")
+dat.acer <-
+  read.xlsx('T:/Systematics/DNA_DATABASES-LIVE/AAA.ACER_Specimen_Database.xlsx') %>% try
+dat.tilia <-
+  read.xlsx('T:/Systematics/DNA_DATABASES-LIVE/AAA.Tilia_Specimen_Database.xlsx') %>% try
+dat.malus <-
+  read.xlsx('T:/Systematics/DNA_DATABASES-LIVE/AAA.Malus_Specimen_Database.xlsx') %>% try
 
-if(!exists('treesToDo')) {
-  stop('Assign the tree(s) you want to relabel as elements of a list in treesToDo and rerun')
-  }
+morTipLabels <- function(
+    treesToDo,
+    dat.meta,
+    labelCols = c(
+      "TAXA-Current_determination",
+      "State", "Country","Collector_no",
+      'RAD-SEQ-FLORAGENEX',
+      'SPMCODE'),
+    matchCol = 'RAD-SEQ-FLORAGENEX',
+    grepCols = TRUE,
+    tipRemovals = '',
+    delim = '|',
+    outfile = NA)
+    {
+  if('phylo' %in% class(treesToDo)) treesToDo <- list(treesToDo)
+  if(length(intersect(class(treesToDo), c('phylo', 'list'))) < 1) {
+    stop('treesToDo should be either a phylo object or a list of phylo objects')
+    } # close if
 
-dat.meta <- read.csv(dat.metaFile)
-treesOut <- treesToDo
-for(i in 1:length(treesOut)) {
-  matchTips <- match(toupper(treesOut[[i]]$tip.label), toupper(dat.meta[[matchCol]]))
-  if(matchTips %>% is.na %>% any) warning(paste('Missing', sum(is.na(matchTips)), 'tips in metadata'))
-  treesOut[[i]]$tip.label <-
-    apply(dat.meta[matchTips, labelCols],
-          1,
-          paste, collapse = delim
-        )
-  rm(matchTips)
-}
+  if(grepCols)
+    labelCols <- sapply(labelCols, function(x) grep(x, names(dat.meta), value = T)[1])
+  treesOut <- treesToDo
+  for(i in 1:length(treesOut)) {
+    matchTips <- match(tidyName(treesOut[[i]]$tip.label), tidyName(dat.meta[[matchCol]]))
+    if(matchTips %>% is.na %>% any) warning(paste('Missing', sum(is.na(matchTips)), 'tips in metadata'))
+    treesOut[[i]]$tip.label <-
+      apply(dat.meta[matchTips, labelCols],
+            1,
+            paste, collapse = delim
+          )
+    rm(matchTips)
+  } # close i
+} # close function
