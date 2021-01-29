2463288cc380b00fc5361bfcc0c224bb72ac3ab7
andrew-hipp <andrew.hipp@gmail.com>
2021-01-28 21:47:45 -0600

    Function complete; adding docs

--- AAA.LAB/morTipLabels.R
+++ AAA.LAB/morTipLabels.R
@@ -2,12 +2,13 @@
 
 require(magrittr)
 require(ape)
+if(!exists('tidyName')) source('https://raw.githubusercontent.com/andrew-hipp/mor-systematics/master/AAA.LAB/tidyName.R')
 
-dat.acer <-
+if(!exists('dat.acer')) dat.acer <-
   read.xlsx('T:/Systematics/DNA_DATABASES-LIVE/AAA.ACER_Specimen_Database.xlsx') %>% try
-dat.tilia <-
+if(!exists('dat.tilia')) dat.tilia <-
   read.xlsx('T:/Systematics/DNA_DATABASES-LIVE/AAA.Tilia_Specimen_Database.xlsx') %>% try
-dat.malus <-
+if(!exists('dat.malus')) dat.malus <-
   read.xlsx('T:/Systematics/DNA_DATABASES-LIVE/AAA.Malus_Specimen_Database.xlsx') %>% try
 
 morTipLabels <- function(
@@ -20,9 +21,16 @@ morTipLabels <- function(
       'SPMCODE'),
     matchCol = 'RAD-SEQ-FLORAGENEX',
     grepCols = TRUE,
-    tipRemovals = '',
+    tipRemovals = '.fq.gzbarcodeStripped',
+    outgroup = NA,
+    outgroupGrep = TRUE,
     delim = '|',
-    outfile = NA)
+    ladder = TRUE,
+    outfile = NA,
+    pdfTree = TRUE,
+    tip.cex = 0.6,
+    nodeLabels = TRUE,
+    pdfW = 10, pdfH = 20)
     {
   if('phylo' %in% class(treesToDo)) treesToDo <- list(treesToDo)
   if(length(intersect(class(treesToDo), c('phylo', 'list'))) < 1) {
@@ -33,6 +41,8 @@ morTipLabels <- function(
     labelCols <- sapply(labelCols, function(x) grep(x, names(dat.meta), value = T)[1])
   treesOut <- treesToDo
   for(i in 1:length(treesOut)) {
+    treesOut[[i]]$tip.label <-
+      gsub(tipRemovals, '', treesOut[[i]]$tip.label)
     matchTips <- match(tidyName(treesOut[[i]]$tip.label), tidyName(dat.meta[[matchCol]]))
     if(matchTips %>% is.na %>% any) warning(paste('Missing', sum(is.na(matchTips)), 'tips in metadata'))
     treesOut[[i]]$tip.label <-
@@ -41,5 +51,26 @@ morTipLabels <- function(
             paste, collapse = delim
           )
     rm(matchTips)
+    if(ladder) treesOut[[i]] <- ladderize(treesOut[[i]])
+    if(outgroupGrep & !is.na(outgroup[1])) {
+      outgroup = grep(outgroup, treesOut[[1]]$tip.label, value = T) %>%
+        as.character
+    } # close outgroup
+    if(!is.na(outgroup[1])) {
+      treesOut[[i]] <- try(root(treesOut[[i]], outgroup))
+      message('Rooting by this outgroup:')
+      message(outgroup)}
+
   } # close i
+  if(!is.na(outfile)) {
+    for(i in 1:length(treesOut)) {
+      write.tree(treesOut[[i]], paste(outfile, i, 'tre', sep = '.'))
+      if(pdfTree) {
+        pdf(paste(outfile, i, 'pdf', sep = '.'), pdfW, pdfH)
+        plot(treesOut[[i]], cex = tip.cex, show.node.label = nodeLabels)
+        dev.off()
+      }
+    } # close write.tree
+  } # close if(!is.na(outfile))
+  return(treesOut)
 } # close function
