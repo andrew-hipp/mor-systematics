77b6165562ddbba8156bffdafcfcf5f9e70924b6
andrew-hipp <andrew.hipp@gmail.com>
2021-03-01 14:47:21 -0600

    adding parallel option and list simplification

--- AAA.LAB/bpScores.R
+++ AAA.LAB/bpScores.R
@@ -1,20 +1,23 @@
 ## bp table for a set of sequences
 require(magrittr)
 require(seqinr)
-
+require(parallel)
 bpScores <- function(
                       files, seqNames = NA, format = 'fasta',
-                      sortBy = c(amb(), '-', '?')
+                      sortBy = c(amb(), '-', '?'),
+                      ncores = 1, simplify = TRUE
                     )
 {
   if(format != 'fasta') stop('only fasta supported now')
   seqs <- lapply(files, read.fasta)
   if(!identical(seqNames, NA)) names(seqs) <- seqNames
-  seqSum <- sapply(seqs, function(x) {
+  seqSum <- mclapply(seqs, function(x) {
     temp <- x %>% unlist %>% table %>% '['(sortBy)
     names(temp) <- sortBy
+    temp[is.na(temp)] <- 0
     temp
-    })
+  }, mc.cores = ncores)
+  if(simplify) seqSum <- simplify2array(seqSum)
   seqSum
 }
 
