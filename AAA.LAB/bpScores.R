584ada8d3e64020819d0695428cc08f7d2a18de1
andrew-hipp <andrew.hipp@gmail.com>
2021-03-01 14:37:51 -0600

    fixing missing parenthesis

--- AAA.LAB/bpScores.R
+++ AAA.LAB/bpScores.R
@@ -3,17 +3,19 @@ require(magrittr)
 require(seqinr)
 
 bpScores <- function(
-                      files, seqNames = NA,
-                      format = 'fasta', ambProp = TRUE,
+                      files, seqNames = NA, format = 'fasta',
                       sortBy = c(amb(), '-', '?')
                     )
 {
   if(format != 'fasta') stop('only fasta supported now')
   seqs <- lapply(files, read.fasta)
   if(!identical(seqNames, NA)) names(seqs) <- seqNames
-  seqSum <- sapply(seqs, function(x) x %>% unlist %>% table %>%'['(sortBy)
-  names(seqSum) <- sortBy
+  seqSum <- sapply(seqs, function(x) {
+    temp <- x %>% unlist %>% table %>% '['(sortBy)
+    names(temp) <- sortBy
+    })
   seqSum
 }
 
 ## example
+bpSums <- function(scoreMat){}
