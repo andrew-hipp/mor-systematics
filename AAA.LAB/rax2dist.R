5c2f7edd43603419d689d4836e0ef7228451108e
andrew-hipp <andrew.hipp@gmail.com>
2021-03-02 07:40:46 -0600

    Add rax2dist Originally in elm data scripts

---
+++ AAA.LAB/rax2dist.R
@@ -0,0 +1,13 @@
+## convert raxml-style long-format pairwise distance table into a square distance matrix
+
+rax2dist <- function(file, diag.fill = 0) {
+  dat <- read.table(file, as.is = T)
+  allTips <- sort(unique(unlist(dat[1:2])))
+  out <- matrix(NA, length(allTips), length(allTips),
+                dimnames = list(allTips, allTips))
+  for(i in 1:dim(dat)[1]) out[dat[i, 'V1'], dat[i, 'V2']] <-
+                          out[dat[i, 'V2'], dat[i, 'V1']] <-
+                          dat[i, 'V3']
+  diag(out) <- diag.fill
+  out
+  }
