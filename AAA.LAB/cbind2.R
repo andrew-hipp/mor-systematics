e5a227d66a52767e8324c831cf826702f8cc19f7
Andrew Hipp <andre@res326-ah.morton.local>
2021-03-03 06:42:23 -0600

    cbind2 to properly cbind matrices w/ missing data

---
+++ AAA.LAB/cbind2.R
@@ -0,0 +1,13 @@
+cbind2 <- function (x, fill = NA)
+{
+    rows <- unlist(lapply(x, row.names))
+    columns <- sum(sapply(x, function(y) dim(y)[2]))
+    out <- matrix(fill, length(rows), columns, dimnames = list(rows,
+        NULL))
+    index = 1
+    for (i in 1:length(x)) {
+        out[row.names(x[[i]]), index:(index + dim(x[[i]])[2] - 1)] <- x[[i]]
+        index = index + dim(x[[i]])[2]
+    }
+    out
+}
