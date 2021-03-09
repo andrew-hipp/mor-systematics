ee5463f6bed763ed6e74b35bf230295ed5891b4b
andrew-hipp <andrew.hipp@gmail.com>
2021-03-08 22:06:53 -0600

    little bugs and things

--- AAA.LAB/plot.strObj.R
+++ AAA.LAB/plot.strObj.R
@@ -2,6 +2,7 @@ plot.strObj <- function(x, ...){
   require(ggplot2)
   require(data.table)
   xm <- melt(x)
+  xm$sample <- factor(xm$sample, levels = unique(xm$sample))
   names(xm) <- c('Sample', 'Cluster', 'Probability')
   out <- ggplot(xm, aes(x=Sample,y=Probability,fill=Cluster))
   out <- out + geom_bar(stat="identity", position="stack")
--- AAA.LAB/sort.strObj.R
+++ AAA.LAB/sort.strObj.R
@@ -1,5 +1,5 @@
 sort.strObj <- function(x) {
   x.clusters <- grep('cluster', names(x), value = T)
-  for(i in x.clusters) x <- x[order(x[[i]])]
+  for(i in x.clusters) x <- x[order(x[[i]]), ]
   return(x)
 }
