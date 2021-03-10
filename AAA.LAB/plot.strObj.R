5f6b2acb20309335d94d5bce03fe92ad79906222
andrew-hipp <andrew.hipp@gmail.com>
2021-03-10 16:59:01 -0600

    updates to plot.strOjb

--- AAA.LAB/plot.strObj.R
+++ AAA.LAB/plot.strObj.R
@@ -1,23 +1,31 @@
-plot.strObj <- function(x, extras = NULL, addSpBar = FALSE, ...){
-# extras is a data frame, one item per row of plot.strObj
+plot.strObj <- function(x, species = NULL,
+                        clusterNames = NULL, addSpBar = FALSE, ...){
+# species is a vector, one item per row of plot.strObj
+# clusterNames is an optional named vector of form c(cluster1 = 'xx', ...)
   require(ggplot2)
   require(data.table)
-  if(!identical(NULL, extras)) {
-    x <- cbind(x, extras)
+  if(!identical(NULL, clusterNames)) {
+    names(x)[match(names(clusterNames), names(x))] <- clusterNames
+  }
+  if(!identical(NULL, species)) {
+    x <- cbind(x, Species = species)
   }
   xm <- melt(x)
   xm$sample <- factor(xm$sample, levels = unique(xm$sample))
-  names(xm) <- c('Sample', 'Cluster', 'Probability', names(extras))
+  names(xm) <- c('Sample', 'Species', 'Cluster', 'Probability')
   out <- ggplot(xm, aes(x=Sample,y=Probability,fill=Cluster))
   out <- out + geom_bar(stat="identity", position="stack")
   out <- out + theme_classic()
   out <- out + scale_fill_brewer(palette="Spectral")
   out <- out + theme(axis.text.x = element_text(size=5, angle = 90, hjust = 1,colour="black"))
+  # if(spFacet) out <- out + facet_grid(~Species)
   if(addSpBar) {
     out <- out +
-      geom_bar(
-        mapping = aes(x = Species, y = 1.1, linetype = Species),
-        stat = "identity", width = 1
+      geom_segment(
+        data = x,
+        mapping = aes(x = min(Species), xend = max(Species),
+                      y = 1.1, yend = 1.1, fill = Species), #deleted colour = Species
+        width = 1
       ) # close geom_bar
   } # close addPopBar
   return(out)
