5c897c82e61c3ef21f052e0eb2f356402aff2760
andrew-hipp <andrew.hipp@gmail.com>
2021-03-11 14:52:11 -0600

    defaults to resorting by cluster names

--- AAA.LAB/str.clusterNames.R
+++ AAA.LAB/str.clusterNames.R
@@ -3,11 +3,12 @@
 ##  x = output from read.structure
 ##  nameList = list of indiduals by sp
 
-str.rename <- function(x, nameList) {
+str.rename <- function(x, nameList, doSort = TRUE) {
   for(clustCol in 2:length(x)) {
-    temp = sapply(spList, function(x) mean(fsIn[[1]][x, clustCol]))
-    names(x)[clustCol] <- names(nameList)[which(temp == max(temp))]
+    temp = sapply(spList, function(y) mean(x[y, clustCol]))
+    names(x)[clustCol] <- names(nameList)[which(temp == max(temp))[1]]
   }
   names(x) <- make.unique(names(x))
+  if(doSort) x <- x[order(names(x))]
   return(x)
 }
