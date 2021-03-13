c9486c577496602cc3110962c595e705008abaa9
andrew-hipp <andrew.hipp@gmail.com>
2021-03-12 21:39:46 -0600

    colors fixed; add support to drop x label

--- AAA.LAB/plot.strObj.R
+++ AAA.LAB/plot.strObj.R
@@ -1,9 +1,16 @@
 plot.strObj <- function(x, species = NULL,
                         clusterNames = NULL,
                         addSpBar = FALSE,
-                        colorVect = NULL, ...){
+                        colorVect = NULL,
+                        species = NULL,
+                        add.x.lab = FALSE,
+                        ...){
 # species is a vector, one item per row of plot.strObj
 # clusterNames is an optional named vector of form c(cluster1 = 'xx', ...)
+  if(!identical(species, NULL)) {
+    warning('species vector not implemented; dropped for now')
+    species <- NULL
+  }
   require(ggplot2)
   require(data.table)
   if(!identical(NULL, clusterNames)) {
@@ -14,14 +21,16 @@ plot.strObj <- function(x, species = NULL,
   }
   xm <- melt(x)
   xm$sample <- factor(xm$sample, levels = unique(xm$sample))
-  names(xm) <- c('Sample', 'Species', 'Cluster', 'Probability')
+  if(!identical(NULL, species)) {
+    names(xm) <- c('Sample', 'Species', 'Cluster', 'Probability')
+  } else names(xm) <- c('Sample', 'Cluster', 'Probability')
   out <- ggplot(xm, aes(x=Sample,y=Probability,fill=Cluster))
   out <- out + geom_bar(stat="identity", position="stack")
-  out <- out + theme_classic()
+  # out <- out + theme_classic()
   if(identical(colorVect, NULL)) {
     out <- out + scale_fill_brewer(palette="Spectral")
   } else out <- out + scale_fill_manual(values = colorVect)
-  out <- out + theme(axis.text.x = element_text(size=5, angle = 90, hjust = 1,colour="black"))
+  if(add.x.lab) out <- out + theme(axis.text.x = element_text(size=5, angle = 90, hjust = 1,colour="black"))
   # if(spFacet) out <- out + facet_grid(~Species)
   if(addSpBar) {
     warning('addSpBar currently not working properly')
