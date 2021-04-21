71e54235fe4a20a9f9eb831f448804179acd5dd4
andrew-hipp <andrew.hipp@gmail.com>
2021-04-21 16:46:18 -0500

    Filling blanks with NAs; writing skipped IDs

--- AAA.BRAHMS.IO/SCRIPTS/00.readAndFormatSEINet.R
+++ AAA.BRAHMS.IO/SCRIPTS/00.readAndFormatSEINet.R
@@ -12,10 +12,13 @@ library(magrittr)
 doAll <- FALSE  ## if doAll is false it will only pull georeference fields to import, if doAll is TRUE it will grab all the fields listed in fields.noImport and put them into a table.
 readData <- TRUE   ## if TRUE will look for new data field to read. (I believe by the last date??) Will want this to be TRUE when downloading new SEINET file
 Collect_GUID <- TRUE ##MH ADDED ....if TRUE, you've made a new export from the specimen table containing the collection events table GUID and barcode matchup. This will be used in the match-transfer to get it back into BRAHMS8. If False, it uses the previously exported file for this. Note that this is probably ok, except for records that might have been newly data entered.
+editorExclude <- 'Goldberg'
 
 if(readData) {
   dat <- read.csv(dir('../DATA.FROM.SEINET', patt = 'csv', full = T), as.is = T)
   dat <- dat[order(dat$EditId), ]
+  dat$OldValue[dat$OldValue == ''] <- NA
+  dat.orig <- dat
 }
 
 ###MH added. Read in new CollectionEvents to barcode matchup file exported from Specimen table of BRAHMS8. Note this isn't tested yet or implemented.
@@ -25,7 +28,9 @@ if(Collect_GUID) {
 
 
 ## exclude everything edited by Goldberg  ##edited fiels are county and stateProvince changes that we're not going to accept back into BRAHMS because we share those county/state formats with Living Collections
-dat <- dat[grep('Goldberg', dat$Editor, invert = T), ]
+if(!identical(editorExclude, NULL)) {
+  dat <- dat[grep(editorExclude, dat$Editor, invert = T), ]
+}
 
 ## shove everything into the table
 fields.import <- c(
@@ -68,18 +73,18 @@ fields.all <- c(fields.import, fields.noImport)
 
 if(doAll) fields.use <- fields.all
 if(!doAll) fields.use <- fields.import
-
+dat <- dat.orig[which(dat.orig$FieldName %in% fields.use), ]
 barcodes <- dat$CatalogNumber %>% unique %>% sort
 
 out <- out.lastChange <-
   matrix('', length(barcodes), length(fields.use),
           dimnames = list(barcodes, fields.use)
         )
-out.skipped <- dat[0, ]
+out.skipped <- numeric(0)
 for(i in seq(dim(dat)[1])) {
   if(i %/% 5000 == i / 5000)
     message(paste('doing record', i, 'of', dim(dat)[1]))
-  if(!dat$FieldName[i] %in% fields.use) next # skip if not a field we care about
+  # if(!dat$FieldName[i] %in% fields.use) next # skip if not a field we care about
   dt <- dat[i, ]
   if(is.na(dt$OldValue)) {
     out.lastChange[dt$CatalogNumber, dt$FieldName] <-
@@ -91,7 +96,7 @@ for(i in seq(dim(dat)[1])) {
       out[dt$CatalogNumber, dt$FieldName] <- dt$NewValue
       out.lastChange[dt$CatalogNumber, dt$FieldName] <-
         paste(dt$NewValue, dt$Editor, convertToDate(dt$Timestamp), sep = '|')
-    } else out.skipped <- rbind(out.skipped, dt)
+    } else out.skipped <- c(out.skipped, dt$EditId)
   }
 }
 
@@ -111,6 +116,6 @@ out.import$MatchGUID <- CEGUID$CollectionEventId[match(row.names(out.import), CE
 ##There are some notice some blanks in the barcode column, whcih might? match up a blank in this other file to the wrong collection events GUID
 #####################################  OUTPUT FILES ########################################
 write.csv(out, paste('../OUT/FulltableEdits_', stamp, '.csv', sep = ''))  ###This may not be needed. It contains the table version of the edits, and leaves the lat and long blank when we don't accept them, but leaves the row with barcode there.
-write.csv(out.skipped, paste('../OUT/dataLinesSkipped_', stamp, '.csv', sep = ''))
+writeLines(as.character(out.skipped), paste('../OUT/dataEditIdSkipped_', stamp, '.csv', sep = ''))
 write.csv(out.lastChange, paste('../OUT/dataLastEdit_', stamp, '.csv', sep = ''))
 write.csv(out.import, paste('../OUT/importToBrahmsTEST_', stamp, '.csv', sep = ''))  ##MH added THIS SHOULD BE THE FILE USED TO INPUT INTO BRAHMS8
